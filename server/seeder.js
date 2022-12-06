04cc29122a5203745ad83be67c6e28f7b3c2de8f
moh-sa <tno.rappelz@gmail.com>
2022-12-06 20:16:56 +0300

    section 4 - vid 22

---
+++ server/seeder.js
@@ -0,0 +1,46 @@
+import products from "./data/products.js";
+import users from "./data/users.js";
+import User from "./models/userModel.js";
+import Product from "./models/productModel.js";
+import Order from "./models/orderModel.js";
+import connectDB from "./config/db.js";
+
+connectDB();
+
+const importData = async () => {
+  try {
+    const createdUsers = await User.insertMany(users);
+    const adminUser = createdUsers[0]._id;
+
+    const sampleProducts = products.map((p) => {
+      return { ...p, user: adminUser };
+    });
+
+    await Product.insertMany(sampleProducts);
+
+    console.log("Data Imported!");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+const destroyData = async () => {
+  try {
+    await Order.deleteMany();
+    await Product.deleteMany();
+    await User.deleteMany();
+    console.log("Data Destroyed!");
+    process.exit();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+if (process.argv[2] === "-d") {
+  destroyData();
+} else {
+  importData();
+}
