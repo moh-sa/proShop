bc0696db4c31465a08a620c51f5ec711b8c118ac
moh-sa <tno.rappelz@gmail.com>
2022-12-20 19:31:20 +0300

    section 7 - vid 39

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ import express from "express";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middlewares/errorMiddleware.js";
 
 const app = express();
 
 connectDB();
 
+app.use(express.json());
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(express.urlencoded({ extended: true }));
 
@@ -16,6 +18,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 app.use(notFound);
 
