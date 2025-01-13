ea13a588804288981c6a8d9f2c3aff30c5df31cd
moh-sa <tno.rappelz@gmail.com>
2025-01-13 18:10:29 +0300

    add index file to 'services' folder

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { userService } from "../services/user.service";
+import { userService } from "../services";
 import { TInsertUser } from "../types";
 
 class UserController {
