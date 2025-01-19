fcc87b36c8347d5aa5ddb969096b69d2fe39426f
moh-sa <tno.rappelz@gmail.com>
2025-01-19 18:53:10 +0300

    convert 'shipping address' interfaces to zod schemas

--- server/src/types/shipping-address.type.ts
+++ server/src/types/shipping-address.type.ts
@@ -1,6 +1,8 @@
-export interface TShippingAddress {
-  address: string;
-  city: string;
-  postalCode: string;
-  country: string;
-}
+import { z } from "zod";
+
+export const shippingAddressSchema = z.object({
+  address: z.string().min(1, { message: "Address is required." }),
+  city: z.string().min(1, { message: "City is required." }),
+  postalCode: z.string().min(1, { message: "Postal code is required." }),
+  country: z.string().min(1, { message: "Country is required." }),
+});
