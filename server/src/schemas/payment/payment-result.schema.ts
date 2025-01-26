cea9d288917fe614583a5a74557e64e666df3aea
moh-sa <tno.rappelz@gmail.com>
2025-01-26 13:40:27 +0300

    feat: make paymentResultSchema optional

--- server/src/schemas/payment/payment-result.schema.ts
+++ server/src/schemas/payment/payment-result.schema.ts
@@ -1,10 +1,12 @@
 import { z } from "zod";
 
-export const paymentResultSchema = z.object({
-  id: z.string().min(1, { message: "payment ID is required." }),
-  status: z.string().min(1, { message: "payment status is required." }),
-  update_time: z.date(),
-  email_address: z
-    .string()
-    .min(1, { message: "payment email address is required." }),
-});
+export const paymentResultSchema = z
+  .object({
+    id: z.string().min(1, { message: "payment ID is required." }),
+    status: z.string().min(1, { message: "payment status is required." }),
+    update_time: z.date(),
+    email_address: z
+      .string()
+      .min(1, { message: "payment email address is required." }),
+  })
+  .optional();
