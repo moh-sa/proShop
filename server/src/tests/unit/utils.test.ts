292f3f860b0388e2dd17f6c55492408144fcb8f4
Tno <msa@moh-sa.dev>
2025-02-04 17:28:00 +0200

    feat: add unit test to 'formatZodErrors' util

--- server/src/tests/unit/utils.test.ts
+++ server/src/tests/unit/utils.test.ts
@@ -1,8 +1,9 @@
 import assert from "node:assert";
 import test, { describe, suite } from "node:test";
+import { formatZodErrors } from "../../utils";
 import { generateToken } from "../../utils/generateJwtToken";
 import { verifyJwtToken } from "../../utils/verify-jwt-token.util";
-import { mockUser1 } from "../mocks";
+import { mockUser1, mockZodError1, mockZodErrors } from "../mocks";
 
 suite("Util Functions Unit Tests", () => {
   describe("verifyJwtToken", () => {
@@ -27,6 +28,32 @@ suite("Util Functions Unit Tests", () => {
       assert.equal(token.email, mockUser1.insert.email);
       assert.equal(token.password, mockUser1.insert.password);
       assert.equal(token.isAdmin, mockUser1.insert.isAdmin);
+    });
+  });
+
+  describe("formatZodErrors", () => {
+    test("Should return 1 error required name", () => {
+      //@ts-expect-error - it expect the full ZodError object.
+      const result = formatZodErrors(mockZodError1);
+
+      assert.ok(result);
+      assert.equal(result, "user.name Name is required");
+    });
+
+    test("Should return 3 errors", () => {
+      //@ts-expect-error - it expect the full ZodError object.
+      const result = formatZodErrors(mockZodErrors);
+
+      assert.ok(result);
+
+      const errors = result.split("; ");
+      assert.equal(errors.length, 3);
+      assert.equal(errors[0], "user.name Name is required");
+      assert.equal(errors[1], "user.email Invalid email format");
+      assert.equal(
+        errors[2],
+        "user.password Password should be at least 6 characters long",
+      );
     });
   });
 });
