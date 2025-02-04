d08742066cbe7567f8930d2b799eb71e6e05ad3f
Tno <msa@moh-sa.dev>
2025-02-04 20:34:12 +0200

    feat: add mock objectId

--- server/src/tests/mocks/index.ts
+++ server/src/tests/mocks/index.ts
@@ -1,2 +1,3 @@
+export * from "./objectid.mock";
 export * from "./user.mock";
 export * from "./zod-errors.mock";
---
+++ server/src/tests/mocks/objectid.mock.ts
@@ -0,0 +1,5 @@
+import { Types } from "mongoose";
+
+export const mockObjectid1 = new Types.ObjectId("67a25d690f09d1e4582b6e8d");
+export const mockObjectid2 = new Types.ObjectId("507f191e810c19729de860ea");
+export const mockObjectid3 = new Types.ObjectId("6592008029c8c3e4dc76256c");
