8805d38c089b3a21e5fc826d74073c31d54d29c9
Tno <msa@moh-sa.dev>
2025-02-12 22:01:49 +0300

    chore: delete dead code

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -6,6 +6,5 @@ export * from "./handle-error-response.util";
 export * from "./hash-data.util";
 export * from "./is-exist.util";
 export * from "./is-password-valid.util";
-export * from "./is-reviewed.util";
 export * from "./validate-file-type.util";
 export * from "./verify-jwt-token.util";
