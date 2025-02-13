896747a4b0acadff0771e3a0221afca45a42bf21
Tno <msa@moh-sa.dev>
2025-02-13 19:49:42 +0300

    refactor: move 'product' from insert and select to base

--- server/src/schemas/order/order-item.schema.ts
+++ server/src/schemas/order/order-item.schema.ts
@@ -1,20 +1,13 @@
 import { z } from "zod";
 import { objectIdValidator } from "../../validators";
-import {
-  insertProductSchema,
-  selectProductSchema,
-} from "../product/product.schema";
+import { insertProductSchema } from "../product/product.schema";
 
 const baseOrderItemSchema = insertProductSchema
   .pick({ name: true, image: true, price: true })
   .extend({
     qty: z.number().int().min(1).default(1),
+    product: objectIdValidator,
   });
 
-export const insertOrderItemSchema = baseOrderItemSchema.extend({
-  product: objectIdValidator,
-});
-
-export const selectOrderItemSchema = baseOrderItemSchema.extend({
-  product: selectProductSchema,
-});
+export const insertOrderItemSchema = baseOrderItemSchema;
+export const selectOrderItemSchema = baseOrderItemSchema;
