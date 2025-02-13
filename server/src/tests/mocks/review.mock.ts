db3fbc1301617bcf75e63cbdead7a3fb02a1c1fa
Tno <msa@moh-sa.dev>
2025-02-13 20:33:22 +0300

    refactor: add missing 'product' field

--- server/src/tests/mocks/review.mock.ts
+++ server/src/tests/mocks/review.mock.ts
@@ -1,18 +1,20 @@
 import { faker } from "@faker-js/faker";
 import { Types } from "mongoose";
 import { SelectReview } from "../../types";
+import { generateMockObjectId } from "./objectid.mock";
 import { generateMockUser } from "./user.mock";
 
 export function generateMockReview(): SelectReview {
   const user = generateMockUser();
+  const mockId = generateMockObjectId();
 
   return {
     _id: new Types.ObjectId(),
     user: user._id,
+    product: mockId,
     name: user.name,
     rating: faker.number.int({ min: 1, max: 5 }),
     comment: faker.lorem.sentence(),
-    user: user,
     createdAt: new Date(),
     updatedAt: new Date(),
   };
