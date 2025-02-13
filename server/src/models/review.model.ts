63d57bc6f9bd741fe056ba8a8b5011ba6044b919
Tno <msa@moh-sa.dev>
2025-02-13 20:14:38 +0300

    feat: add 'review' mongoDB schema and model

---
+++ server/src/models/review.model.ts
@@ -0,0 +1,35 @@
+import { model, Schema } from "mongoose";
+import { ReviewSchema } from "../types";
+
+const reviewSchema = new Schema<ReviewSchema>(
+  {
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    product: {
+      type: Schema.Types.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const Review = model("Review", reviewSchema);
+export default Review;
