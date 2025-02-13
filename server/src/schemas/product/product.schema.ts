5306c41ed54b6ec77ddff91c42629158c8d72c70
Tno <msa@moh-sa.dev>
2025-02-13 19:45:33 +0300

    refactor: move 'rating' and 'numReviews' to select schema

--- server/src/schemas/product/product.schema.ts
+++ server/src/schemas/product/product.schema.ts
@@ -14,18 +14,6 @@ const baseProductSchema = z.object({
 
   description: z.string().min(1, { message: "Description is required." }),
 
-  rating: z
-    .number()
-    .min(0, { message: "Rating is required." })
-    .max(5, { message: "Rating must be between 1 and 5." })
-    .default(0),
-
-  numReviews: z
-    .number()
-    .int()
-    .min(0, { message: "Number of reviews is required." })
-    .default(0),
-
   price: z.number().min(0, { message: "Price is required." }).default(0),
 
   countInStock: z
@@ -39,6 +27,15 @@ export const insertProductSchema = baseProductSchema;
 
 export const selectProductSchema = baseProductSchema.extend({
   _id: objectIdValidator,
+
+  rating: z
+    .number()
+    .min(0, { message: "Rating is required." })
+    .max(5, { message: "Rating must be between 1 and 5." }),
+  numReviews: z
+    .number()
+    .int()
+    .min(0, { message: "Number of reviews is required." }),
   createdAt: z.date(),
   updatedAt: z.date(),
 });
