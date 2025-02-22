3d1278f56bce87ac85682a144b10198d173c0c96
Tno <msa@moh-sa.dev>
2025-02-22 19:00:11 +0300

    refactor: split 'router' into separate 'public', 'user', and 'admin' sub-routers

--- server/src/routes/v1/reviews.routes.ts
+++ server/src/routes/v1/reviews.routes.ts
@@ -8,9 +8,21 @@ import {
   verifyReviewOwnership,
 } from "../../middlewares";
 
-const router = express.Router();
+const baseRouter = express.Router();
+const publicRouter = express.Router();
+const protectedRoutes = express.Router();
+const userRouter = express.Router();
+const adminRouter = express.Router();
 
-router
+publicRouter
+  .route("/product/:productId")
+  .get(RateLimiterMiddleware.defaultLimiter(), controller.getAllByProductId);
+
+publicRouter
+  .route("/count/product/:productId")
+  .get(RateLimiterMiddleware.defaultLimiter(), controller.countByProductId);
+
+userRouter
   .route("/")
   .post(
     RateLimiterMiddleware.strictLimiter(),
@@ -19,76 +31,31 @@ router
     controller.create,
   );
 
-router.get(
-  "/user/:userId",
-  RateLimiterMiddleware.defaultLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  controller.getAllByUserId,
-);
-
-router.get(
-  "/product/:productId",
-  RateLimiterMiddleware.defaultLimiter(),
-  controller.getAllByProductId,
-);
-
-router.get(
-  "/count/user/:userId",
-  RateLimiterMiddleware.defaultLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  controller.countByUserId,
-);
-router.get(
-  "/count/product/:productId",
-  RateLimiterMiddleware.defaultLimiter(),
-  controller.countByProductId,
-);
-
-router.get(
-  "/exists/user/:userId/product/:productId",
-  RateLimiterMiddleware.defaultLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  controller.existsByUserIdAndProductId,
-);
-
-router.get(
-  "/admin",
-  RateLimiterMiddleware.adminLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  checkIfUserIsAdmin,
-  controller.getAll,
-);
-
-router.get(
-  "/admin/count",
-  RateLimiterMiddleware.adminLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  checkIfUserIsAdmin,
-  controller.count,
-);
-
-router.get(
-  "/admin/exists/:reviewId",
-  RateLimiterMiddleware.adminLimiter(),
-  checkJwtTokenValidation,
-  checkUserIdExists,
-  checkIfUserIsAdmin,
-  controller.existsById,
-);
-
-router
-  .route("admin/:reviewId")
+userRouter
+  .route("/count/user/:userId")
   .get(
-    RateLimiterMiddleware.adminLimiter(),
+    RateLimiterMiddleware.defaultLimiter(),
     checkJwtTokenValidation,
     checkUserIdExists,
-    checkIfUserIsAdmin,
-    controller.getById,
+    controller.countByUserId,
+  );
+
+userRouter
+  .route("/exists/user/:userId/product/:productId")
+  .get(
+    RateLimiterMiddleware.defaultLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    controller.existsByUserIdAndProductId,
+  );
+
+userRouter
+  .route("/:userId")
+  .get(
+    RateLimiterMiddleware.defaultLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    controller.getAllByUserId,
   )
   .patch(
     RateLimiterMiddleware.strictLimiter(),
@@ -105,4 +72,50 @@ router
     controller.delete,
   );
 
-export default router;
+adminRouter
+  .route("/")
+  .get(
+    RateLimiterMiddleware.adminLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    checkIfUserIsAdmin,
+    controller.getAll,
+  );
+
+adminRouter
+  .route("/count")
+  .get(
+    RateLimiterMiddleware.adminLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    checkIfUserIsAdmin,
+    controller.count,
+  );
+
+adminRouter
+  .route("/exists/:reviewId")
+  .get(
+    RateLimiterMiddleware.adminLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    checkIfUserIsAdmin,
+    controller.existsById,
+  );
+
+adminRouter
+  .route("/:reviewId")
+  .get(
+    RateLimiterMiddleware.adminLimiter(),
+    checkJwtTokenValidation,
+    checkUserIdExists,
+    checkIfUserIsAdmin,
+    controller.getById,
+  );
+
+protectedRoutes.use("/", userRouter);
+protectedRoutes.use("/admin", adminRouter);
+
+baseRouter.use("/", publicRouter);
+baseRouter.use("/", protectedRoutes);
+
+export default baseRouter;
