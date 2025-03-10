2ea9bb4b096ff159309adf9bd7aa862b49fcd024
Tno <msa@moh-sa.dev>
2025-03-11 00:08:55 +0300

    feat: add missing zod validation

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,3 +1,4 @@
+import { insertUserSchema, selectUserSchema } from "../schemas";
 import { authService } from "../services";
 import { asyncHandler } from "../utils";
 
@@ -5,13 +6,21 @@ class AuthController {
   private readonly service = authService;
 
   signin = asyncHandler(async (req, res) => {
-    const response = await this.service.signin(res.locals.user);
+    const data = res.locals.user || req.body;
+
+    const parsedData = selectUserSchema
+      .pick({ email: true, password: true })
+      .parse(data);
+
+    const response = await this.service.signin(parsedData);
 
     res.status(200).json(response);
   });
 
   signup = asyncHandler(async (req, res) => {
-    const response = await this.service.signup(req.body);
+    const data = insertUserSchema.parse(req.body);
+
+    const response = await this.service.signup(data);
 
     res.status(201).json(response);
   });
