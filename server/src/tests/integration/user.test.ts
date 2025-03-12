f3d71918be25f680fab34dc5ff520271d4f44e16
Tno <msa@moh-sa.dev>
2025-03-13 00:14:28 +0300

    refactor: update the integration test to reflect the changes

--- server/src/tests/integration/user.test.ts
+++ server/src/tests/integration/user.test.ts
@@ -1,92 +1,123 @@
 import assert from "node:assert";
 import test, { after, before, beforeEach, describe, suite } from "node:test";
 import { authController, userController } from "../../controllers";
-import {
-  checkEmailExists,
-  checkJwtTokenValidation,
-  checkPasswordValidation,
-  checkUserIdExists,
-} from "../../middlewares";
+import { NotFoundError } from "../../errors";
 import User from "../../models/userModel";
-import { SelectUser } from "../../types";
-import { generateToken } from "../../utils";
-import { generateMockUsers } from "../mocks";
+import { generateMockUser } from "../mocks";
 import { createMockExpressContext, dbClose, dbConnect } from "../utils";
 
 before(async () => await dbConnect());
 after(async () => await dbClose());
 beforeEach(async () => await User.deleteMany({}));
 
+// TODO: use 'supertest' to test the routes instead of controllers.
+
 suite("User Integration Tests", () => {
   describe("User Lifecycle", () => {
     test("Should create, authenticate, and update user", async () => {
-      const [mockUser1, mockUser2] = generateMockUsers(2);
+      const mockUser = generateMockUser();
 
-      // 1) Signup
-      const step1 = createMockExpressContext();
-      // 1.1) Check if user exists
-      step1.req.body.email = mockUser1.email;
-      checkEmailExists()(step1.req, step1.res, step1.next);
+      // Step 1: User signup
+      const step1Signup = createMockExpressContext();
+      step1Signup.req.body = mockUser;
 
-      // 1.2) Create user
-      const user = (await User.create(mockUser1)).toObject();
-      assert.ok(user);
-      assert.ok(user._id);
-      assert.equal(user.email, mockUser1.email);
+      await authController.signup(
+        step1Signup.req,
+        step1Signup.res,
+        step1Signup.next,
+      );
 
-      // 2) Generate JWT
-      const jwt = generateToken({ id: user._id });
-      assert.ok(jwt);
+      const step1Res = step1Signup.res._getJSONData();
+      assert.ok(step1Res);
+      assert.equal(step1Signup.res.statusCode, 201);
+      assert.equal(step1Res.email, mockUser.email);
 
-      // 3) Signin
-      const step3 = createMockExpressContext();
-      // 3.1) Check if user exists
-      step3.req.body.email = user.email;
-      await checkEmailExists(true)(step3.req, step3.res, step3.next);
+      // Step 2: User Signin
+      const step2Signin = createMockExpressContext();
+      step2Signin.req.body = {
+        email: mockUser.email,
+        password: mockUser.password,
+      };
 
-      // 3.2) Match passwords
-      step3.req.body.password = mockUser1.password;
-      step3.res.locals.user.password = user.password;
-      await checkPasswordValidation(step3.req, step3.res, step3.next);
+      await authController.signin(
+        step2Signin.req,
+        step2Signin.res,
+        step2Signin.next,
+      );
 
-      // 3.3) Signin
-      step3.res.locals.user = user;
-      await authController.signin(step3.req, step3.res, step3.next);
-      const signinResponse = step3.res._getJSONData() as SelectUser;
-      assert.ok(signinResponse);
-      assert.equal(signinResponse.email, user.email);
-      assert.ok(signinResponse.token);
+      const step2Res = step2Signin.res._getJSONData();
+      assert.ok(step2Res);
+      assert.equal(step2Signin.res.statusCode, 200);
+      assert.equal(step2Res.email, mockUser.email);
 
-      // 4) Get user profile
-      const call4 = createMockExpressContext();
-      // 4.1) extract and validate JWT
-      call4.req.headers.authorization = `Bearer ${jwt}`;
-      checkJwtTokenValidation(call4.req, call4.res, call4.next);
+      const token = step2Res.token;
+      assert.ok(token);
 
-      // 4.2) Check if user exists
-      await checkUserIdExists(call4.req, call4.res, call4.next);
+      // Step 3: Get user profile
+      const step3GetById = createMockExpressContext();
+      step3GetById.req.params.userId = step1Res._id;
 
-      // 4.3) Fetch the user data
-      await userController.getById(call4.req, call4.res, call4.next);
-      const profileResponse = call4.res._getJSONData() as SelectUser;
-      assert.ok(profileResponse);
-      assert.equal(profileResponse.email, user.email);
+      await userController.getById(
+        step3GetById.req,
+        step3GetById.res,
+        step3GetById.next,
+      );
 
-      // 5) Update user
-      const call5 = createMockExpressContext();
-      // 5.1) extract and validate JWT
-      call5.req.headers.authorization = `Bearer ${jwt}`;
-      await checkJwtTokenValidation(call5.req, call5.res, call5.next);
+      const step3Res = step3GetById.res._getJSONData();
+      assert.ok(step3Res);
+      assert.equal(step3GetById.res.statusCode, 200);
+      assert.equal(step3Res.email, mockUser.email);
 
-      // 5.2) Check if user exists
-      await checkUserIdExists(call5.req, call5.res, call5.next);
+      // Step 4: Update user profile
+      const step4Update = createMockExpressContext();
+      step4Update.req.params.userId = step1Res._id;
+      step4Update.req.body = {
+        name: "John Doe",
+        email: "john.doe@gmail.com",
+      };
 
-      // 5.3) update
-      call5.req.body.name = mockUser2.name;
-      await userController.update(call5.req, call5.res, call5.next);
-      const updatedData = call5.res._getJSONData() as SelectUser;
-      assert.ok(updatedData);
-      assert.equal(updatedData.name, mockUser2.name);
+      await userController.update(
+        step4Update.req,
+        step4Update.res,
+        step4Update.next,
+      );
+
+      const step4Res = step4Update.res._getJSONData();
+      assert.ok(step4Res);
+      assert.equal(step4Update.res.statusCode, 200);
+      assert.equal(step4Res.name, "John Doe");
+      assert.equal(step4Res.email, "john.doe@gmail.com");
+
+      // Step 5: Delete user profile
+      const step5Delete = createMockExpressContext();
+      step5Delete.req.params.userId = step1Res._id;
+
+      await userController.delete(
+        step5Delete.req,
+        step5Delete.res,
+        step5Delete.next,
+      );
+
+      const step5Res = step5Delete.res._getJSONData();
+      assert.ok(step5Res);
+      assert.equal(step5Delete.res.statusCode, 204);
+      assert.equal(step5Res.message, "User removed");
+
+      // Step 6: Try to get user profile
+      const step6GetById = createMockExpressContext();
+      step6GetById.req.params.userId = step1Res._id;
+
+      try {
+        await userController.getById(
+          step6GetById.req,
+          step6GetById.res,
+          step6GetById.next,
+        );
+      } catch (error) {
+        assert.ok(error instanceof NotFoundError);
+        assert.equal(error.statusCode, 404);
+        assert.equal(error.message, "User not found");
+      }
     });
   });
 });
