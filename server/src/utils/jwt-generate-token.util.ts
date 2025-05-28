f2c297cf9e1660cea564479813584706b7f1e5ff
Tno <msa@moh-sa.dev>
2025-05-28 23:47:35 +0300

    feat: add default value to 'data'

--- server/src/utils/jwt-generate-token.util.ts
+++ server/src/utils/jwt-generate-token.util.ts
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env";
 
-export const generateJwtToken = (data: Record<string, unknown>) => {
+export const generateJwtToken = (
+  data: Record<string, unknown> = {},
+): string => {
   return jwt.sign(data, env.JWT_SECRET, { expiresIn: "30d" });
 };
