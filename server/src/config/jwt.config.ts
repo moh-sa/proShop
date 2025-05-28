daf0395b3a396affb8d5d58c7b83905c9eb1b8c1
Tno <msa@moh-sa.dev>
2025-05-28 23:55:37 +0300

    feat: add 'options' parameter and default value

---
+++ server/src/config/jwt.config.ts
@@ -0,0 +1,5 @@
+import { SignOptions } from "jsonwebtoken";
+
+export const jwtSignOptions: SignOptions = {
+  expiresIn: "30d",
+};
--- server/src/utils/jwt-generate-token.util.ts
+++ server/src/utils/jwt-generate-token.util.ts
@@ -1,8 +1,10 @@
 import jwt from "jsonwebtoken";
 import { env } from "../config/env";
+import { jwtSignOptions } from "../config/jwt.config";
 
 export const generateJwtToken = (
   payload: Record<string, unknown> = {},
+  options: jwt.SignOptions = jwtSignOptions,
 ): string => {
-  return jwt.sign(payload, env.JWT_SECRET, { expiresIn: "30d" });
+  return jwt.sign(payload, env.JWT_SECRET, options);
 };
