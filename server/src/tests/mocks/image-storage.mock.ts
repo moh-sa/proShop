55ee3bf4294ddc077436571e01b78a1db61e185c
Tno <msa@moh-sa.dev>
2025-05-29 00:45:13 +0300

    test: refactor 'image storage' mock to be flexable and resettable

--- server/src/tests/mocks/image-storage.mock.ts
+++ server/src/tests/mocks/image-storage.mock.ts
@@ -1,11 +1,20 @@
-import { faker } from "@faker-js/faker";
 import { mock } from "node:test";
 import { IImageStorageManager } from "../../managers";
+import { FunctionMocksWithReset } from "../types/mocked.type";
 
-export function mockImageStorage(): IImageStorageManager {
+export function mockImageStorage(): FunctionMocksWithReset<IImageStorageManager> {
   return {
-    upload: mock.fn(() => Promise.resolve(faker.image.url())),
-    delete: mock.fn(() => Promise.resolve()),
-    replace: mock.fn(() => Promise.resolve(faker.image.url())),
+    upload: mock.fn(),
+    replace: mock.fn(),
+    delete: mock.fn(),
+    reset: function () {
+      this.upload.mock.resetCalls();
+      this.replace.mock.resetCalls();
+      this.delete.mock.resetCalls();
+
+      this.upload.mock.restore();
+      this.replace.mock.restore();
+      this.delete.mock.restore();
+    },
   };
 }
