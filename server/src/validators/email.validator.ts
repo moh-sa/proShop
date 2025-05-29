88d084ebb1c10d8ec9dd43f0c2808cad9f522dcc
Tno <msa@moh-sa.dev>
2025-05-29 17:16:42 +0300

    refactor: add 'trim' and 'toLowerCase'

--- server/src/validators/email.validator.ts
+++ server/src/validators/email.validator.ts
@@ -2,5 +2,7 @@ import { z } from "zod";
 
 export const emailValidator = z
   .string()
+  .trim()
+  .toLowerCase()
   .min(1, { message: "Email is required." })
   .email({ message: "Invalid email format." });
