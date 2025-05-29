4dd49333e3267792a20892e82e690157732d8140
Tno <msa@moh-sa.dev>
2025-05-29 18:28:29 +0300

    refactor: use 'z.preprocess' for a cleaner and easier to read schema

--- server/src/validators/object-id.validator.ts
+++ server/src/validators/object-id.validator.ts
@@ -1,11 +1,16 @@
 import { Types } from "mongoose";
 import { z } from "zod";
 
-export const objectIdValidator = z.union([
-  z.instanceof(Types.ObjectId, { message: "Invalid ObjectId format." }),
-  z
-    .string()
-    .min(1, { message: "Required field is missing." })
-    .refine(Types.ObjectId.isValid, { message: "Invalid ObjectId format." })
-    .transform((id) => new Types.ObjectId(id)),
-]);
+export const objectIdValidator = z.preprocess((val) => {
+  if (val instanceof Types.ObjectId) return val;
+
+  if (typeof val === "string") {
+    const trimmed = val.trim();
+
+    if (Types.ObjectId.isValid(trimmed)) {
+      return new Types.ObjectId(trimmed);
+    }
+  }
+
+  return val;
+}, z.instanceof(Types.ObjectId, { message: "Invalid ObjectId format." }));
