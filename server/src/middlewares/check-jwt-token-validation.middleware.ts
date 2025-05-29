725b4b6be92f8545fffea139b64562c030f1dedb
Tno <msa@moh-sa.dev>
2025-05-29 15:43:36 +0300

    refactor: remove redundant step since 'verifyJwtToken' now uses 'jwtTokenValidator' internally

--- server/src/middlewares/check-jwt-token-validation.middleware.ts
+++ server/src/middlewares/check-jwt-token-validation.middleware.ts
@@ -1,10 +1,6 @@
 import { selectUserSchema } from "../schemas";
 import { asyncHandler, verifyJwtToken } from "../utils";
-import {
-  bearerTokenValidator,
-  jwtTokenValidator,
-  objectIdValidator,
-} from "../validators";
+import { bearerTokenValidator, objectIdValidator } from "../validators";
 
 // TODO: rename 'id' to 'userId' or '_id' across the app
 const schema = selectUserSchema.pick({ _id: true }).transform((data) => {
@@ -18,8 +14,7 @@ const schema = selectUserSchema.pick({ _id: true }).transform((data) => {
  */
 export const checkJwtTokenValidation = asyncHandler(async (req, res, next) => {
   const authHeader = bearerTokenValidator.parse(req.headers.authorization);
-  const token = jwtTokenValidator.parse(authHeader);
-  const decoded = verifyJwtToken(token, schema);
+  const decoded = verifyJwtToken(authHeader, schema);
   const userId = objectIdValidator.parse(decoded.id);
 
   res.locals.token = {
