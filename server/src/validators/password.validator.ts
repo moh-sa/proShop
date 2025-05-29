b435a0e94c7695f422f113138f8837a63a5a3eef
Tno <msa@moh-sa.dev>
2025-05-29 18:33:40 +0300

    refactor: add 'trim' and 'max' to 'passwordValidator'

--- server/src/validators/password.validator.ts
+++ server/src/validators/password.validator.ts
@@ -2,4 +2,6 @@ import { z } from "zod";
 
 export const passwordValidator = z
   .string()
-  .min(6, { message: "Password should be at least 6 characters long." });
+  .trim()
+  .min(6, { message: "Password should be at least 6 characters long." })
+  .max(128, { message: "Password should be at most 128 characters long." });
