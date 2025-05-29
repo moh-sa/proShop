8b94dbcdffb4a6fdf714ea18a2fb4a47168686c4
Tno <msa@moh-sa.dev>
2025-05-29 17:48:35 +0300

    refactor: add 'trim' to 'jwtTokenValidator'

--- server/src/validators/jwt-token.validator.ts
+++ server/src/validators/jwt-token.validator.ts
@@ -2,5 +2,6 @@ import { z } from "zod";
 
 export const jwtTokenValidator = z
   .string()
+  .trim()
   .min(1, { message: "Token is required." })
   .jwt({ message: "Invalid jwt token format." });
