78a39434c066cb582be10d010276c46747208163
Tno <msa@moh-sa.dev>
2025-07-26 16:08:50 +0300

    chore: remove `rate-limit.middleware` export from middleware index

--- server/src/middlewares/index.ts
+++ server/src/middlewares/index.ts
@@ -2,5 +2,4 @@ export * from "./check-jwt-token-validation.middleware";
 export * from "./check-product-reviewed-by-user.middleware";
 export * from "./check-user-id-exists.middleware";
 export * from "./check-user-is-admin.middleware";
-export * from "./rate-limit.middleware";
 export * from "./verify-review-ownership.middleware";
