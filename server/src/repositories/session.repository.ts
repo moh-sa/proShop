17c27b352e81f9a7aad9388d52a5895cb8911d3b
Tno <msa@moh-sa.dev>
2025-08-18 18:14:53 +0300

    feat: add `create` method in `SessionRepository`

--- server/src/repositories/session.repository.ts
+++ server/src/repositories/session.repository.ts
@@ -1,9 +1,11 @@
-import type { FailureResult } from "../types/index.js";
+import type { InsertSession, SelectSession } from "../types/index.js";
 
 import { Session } from "../models/session.model.js";
 import { handleDatabaseError } from "../utils/index.js";
 
-export interface ISessionRepository {}
+export interface ISessionRepository {
+	create(args: InsertSession): Promise<SelectSession>;
+}
 
 export class SessionRepository implements ISessionRepository {
 	private readonly _db: typeof Session;
@@ -12,7 +14,15 @@ export class SessionRepository implements ISessionRepository {
 		this._db = db ?? Session;
 	}
 
-	private _errorHandler(error: unknown): FailureResult {
+	public async create(args: InsertSession): Promise<SelectSession> {
+		try {
+			return (await this._db.create(args)).toObject();
+		} catch (error) {
+			return this._errorHandler(error);
+		}
+	}
+
+	private _errorHandler(error: unknown): never {
 		return handleDatabaseError(error);
 	}
 }
