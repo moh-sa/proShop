ac29476f7bcefd1e9f30f35d020941dae37c34b4
Tno <msa@moh-sa.dev>
2025-08-21 17:38:29 +0300

    feat: add password hashing method to `PasswordService`

--- server/src/services/password.service.ts
+++ server/src/services/password.service.ts
@@ -1,17 +1,29 @@
 import * as argon from "argon2";
 import { z } from "zod";
 
-import { PasswordValidationError } from "../errors/index.js";
+import { PasswordHashError, PasswordValidationError } from "../errors/index.js";
 import { formatZodErrors } from "../utils/index.js";
 import { passwordValidator } from "../validators/index.js";
 
-export interface IPasswordService {}
+export interface IPasswordService {
+	hash(args: { password: string }): Promise<string>;
+}
 
 export class PasswordService implements IPasswordService {
 	private readonly _provider: typeof argon;
 
 	constructor(provider: typeof argon = argon) {
 		this._provider = provider;
+	}
+
+	public async hash(args: { password: string }): Promise<string> {
+		this._validate(args);
+
+		try {
+			return await this._provider.hash(args.password);
+		} catch (error) {
+			throw new PasswordHashError({ cause: error });
+		}
 	}
 
 	private _validate(args: { hashedPassword?: string; password: string }): void {
