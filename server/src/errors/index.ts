9f1f9bf4ebe2902dd5b0042807c75f5702f7f84e
Tno <msa@moh-sa.dev>
2025-08-24 13:23:21 +0300

    feat: add base-error class for `jwt-specific` errors

--- server/src/errors/index.ts
+++ server/src/errors/index.ts
@@ -11,6 +11,7 @@ export * from "./jwt-expired-token.error.js";
 export * from "./jwt-invalid-payload.error.js";
 export * from "./jwt-invalid-token.error.js";
 export * from "./jwt-verification.error.js";
+export * from "./jwt/index.js";
 export * from "./not-found.error.js";
 export * from "./password/index.js";
 export * from "./rate-limit.error.js";
