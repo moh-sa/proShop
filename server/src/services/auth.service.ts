9221ef6908d6b900078628a811629ab889cad457
Tno <msa@moh-sa.dev>
2025-08-24 13:48:38 +0300

    feat: integrate `JwtService` into `AuthService`

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,10 +1,12 @@
 import type { IUserRepository } from "../repositories/index.js";
 import type { InsertUser, RequiredBy, SelectUser } from "../types/index.js";
+import type { IJwtService } from "./jwt.service.js";
 import type { IPasswordService } from "./password.service.js";
 
 import { AuthenticationError } from "../errors/index.js";
 import { UserRepository } from "../repositories/index.js";
 import { generateJwtToken, removeObjectFields } from "../utils/index.js";
+import { JwtService } from "./jwt.service.js";
 import { PasswordService } from "./password.service.js";
 
 export interface IAuthService {
@@ -14,15 +16,18 @@ export interface IAuthService {
 	signup: (data: InsertUser) => Promise<Omit<SelectUser, "password">>;
 }
 export class AuthService implements IAuthService {
+	private readonly _jwtService: IJwtService;
 	private readonly _passwordService: IPasswordService;
 	private readonly _repository: IUserRepository;
 
 	constructor(
 		repository: IUserRepository = new UserRepository(),
 		passwordService: IPasswordService = new PasswordService(),
+		jwtService: IJwtService = new JwtService(),
 	) {
 		this._repository = repository;
 		this._passwordService = passwordService;
+		this._jwtService = jwtService;
 	}
 
 	async signin(data: RequiredBy<SelectUser, "email" | "password">) {
