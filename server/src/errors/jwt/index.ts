2c21c12effcae81b74ea7a831dcd2b0bdada42d2
Tno <msa@moh-sa.dev>
2025-08-24 13:18:25 +0300

    feat: add `jwt invalid payload` error class

--- server/src/errors/jwt/index.ts
+++ server/src/errors/jwt/index.ts
@@ -1,3 +1,4 @@
 export * from "./jwt-base.error.js";
 export * from "./jwt-expiration.error.js";
 export * from "./jwt-generation.error.js";
+export * from "./jwt-invalid-payload.error.js";
---
+++ server/src/errors/jwt/jwt-invalid-payload.error.ts
@@ -0,0 +1,13 @@
+import { ErrorType } from "../../types/index.js";
+import { JwtBaseError } from "./jwt-base.error.js";
+
+export class JwtInvalidPayloadError extends JwtBaseError {
+	constructor(details: Record<string, unknown> = {}) {
+		super(
+			"Invalid JWT token payload",
+			ErrorType.JWT_INVALID_PAYLOAD,
+			401,
+			details,
+		);
+	}
+}
