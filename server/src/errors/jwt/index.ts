6f3d2e3a5bd8721c7d5342785cae8f218fccb825
Tno <msa@moh-sa.dev>
2025-08-24 13:18:37 +0300

    feat: add `jwt invalid token` error class

--- server/src/errors/jwt/index.ts
+++ server/src/errors/jwt/index.ts
@@ -2,3 +2,4 @@ export * from "./jwt-base.error.js";
 export * from "./jwt-expiration.error.js";
 export * from "./jwt-generation.error.js";
 export * from "./jwt-invalid-payload.error.js";
+export * from "./jwt-invalid-token.error.js";
---
+++ server/src/errors/jwt/jwt-invalid-token.error.ts
@@ -0,0 +1,13 @@
+import { ErrorType } from "../../types/index.js";
+import { JwtBaseError } from "./jwt-base.error.js";
+
+export class JwtInvalidTokenError extends JwtBaseError {
+	constructor(details: Record<string, unknown> = {}) {
+		super(
+			"Invalid JWT token format",
+			ErrorType.JWT_INVALID_TOKEN,
+			401,
+			details,
+		);
+	}
+}
