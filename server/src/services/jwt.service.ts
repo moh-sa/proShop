75e944c2c8b658fbb5e759f6840591314d24a6ca
Tno <msa@moh-sa.dev>
2025-08-24 13:31:23 +0300

    feat: add `user ID` validation in `JwtService`

--- server/src/services/jwt.service.ts
+++ server/src/services/jwt.service.ts
@@ -1,9 +1,10 @@
 import jwt from "jsonwebtoken";
+import { z } from "zod";
 
-import type { JwtBaseError } from "../errors/index.js";
 import type { JwtConfig, Result } from "../types/index.js";
 
 import { DEFAULT_JWT_CONFIG } from "../config/index.js";
+import { type JwtBaseError, JwtInvalidPayloadError } from "../errors/index.js";
 
 export interface IJwtService {}
 
@@ -19,5 +20,24 @@ export class JwtService implements IJwtService {
 	) {
 		this._config = config;
 		this._provider = provider;
+	}
+
+	private _validateUserId(userId: string): JwtResult<string> {
+		const result = z
+			.string()
+			.trim()
+			.min(1, "User ID is required")
+			.safeParse(userId);
+
+		if (!result.success) {
+			return {
+				error: new JwtInvalidPayloadError({ cause: result.error }),
+				success: false,
+			};
+		}
+		return {
+			data: userId,
+			success: true,
+		};
 	}
 }
