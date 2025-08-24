9648a4d168c8e46edfdd6b3f000cb88e870d5869
Tno <msa@moh-sa.dev>
2025-08-24 13:15:35 +0300

    feat: add default configuration for JWT

--- server/src/config/index.ts
+++ server/src/config/index.ts
@@ -1,4 +1,5 @@
 export * from "./cache.config.js";
 export * from "./db.js";
 export * from "./env.js";
+export * from "./jwt.config.js";
 export * from "./rate-limit.config.js";
--- server/src/config/jwt.config.ts
+++ server/src/config/jwt.config.ts
@@ -1,6 +1,17 @@
 import type { SignOptions } from "jsonwebtoken";
 
+import type { JwtConfig } from "../types/index.js";
+
+import { env } from "./env.js";
+
 /** @deprecated*/
 export const jwtSignOptions: SignOptions = {
 	expiresIn: "30d",
+};
+
+export const DEFAULT_JWT_CONFIG: JwtConfig = {
+	accessTokenExpiresIn: 15 * 60, // 15 minutes
+	accessTokenSecret: env.JWT_ACCESS_TOKEN_SECRET,
+	refreshTokenExpiresIn: 30 * 24 * 60 * 60, // 30 days
+	refreshTokenSecret: env.JWT_REFRESH_TOKEN_SECRET,
 };
