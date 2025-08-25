99ab72949c7dd151d2cd24b6676db64dd0f1ac2c
Tno <msa@moh-sa.dev>
2025-08-25 12:33:22 +0300

    test: add integration test skeleton for `JWT Service`

---
+++ server/src/tests/integration/jwt.service.integration.test.ts
@@ -0,0 +1,14 @@
+import { describe, suite } from "node:test";
+
+import { JwtService } from "../../services/index.js";
+
+suite("JWT Service〖 Integration Tests 〗", { todo: "IMPLEMENT" }, () => {
+	const service = new JwtService(); // eslint-disable-line @typescript-eslint/no-unused-vars
+
+	// I AM TIRED BOSS :')
+	describe("generateAccessToken", () => {});
+	describe("generateRefreshToken", () => {});
+	describe("generateTokenPair", () => {});
+	describe("refreshAccessToken", () => {});
+	describe("verify", () => {});
+});
