3d0cbaece54c5280c7c13ebb406d57c6516ed57c
Tno <msa@moh-sa.dev>
2025-08-30 18:30:38 +0300

    feat: add `create` public method in `SessionService`

--- server/src/services/session.service.ts
+++ server/src/services/session.service.ts
@@ -1,10 +1,22 @@
 import type { ISessionRepository } from "../repositories/session.repository.js";
-import type { FailureResult, Result } from "../types/index.js";
+import type {
+	FailureResult,
+	InsertSession,
+	Result,
+	SelectSession,
+} from "../types/index.js";
 
-import { BaseError, SessionBaseError } from "../errors/index.js";
+import {
+	BaseError,
+	DatabaseDuplicateKeyError,
+	SessionAlreadyExistsError,
+	SessionBaseError,
+} from "../errors/index.js";
 import { SessionRepository } from "../repositories/index.js";
 
-export interface ISessionService {}
+export interface ISessionService {
+	create(args: InsertSession): Promise<SessionResult<SelectSession>>;
+}
 
 type SessionResult<T> = Result<T, SessionBaseError>;
 
@@ -13,6 +25,28 @@ export class SessionService implements ISessionService {
 
 	constructor(repository: ISessionRepository = new SessionRepository()) {
 		this._repository = repository;
+	}
+
+	public async create(
+		args: InsertSession,
+	): Promise<SessionResult<SelectSession>> {
+		try {
+			const session = await this._repository.create(args);
+
+			return {
+				data: session,
+				success: true,
+			};
+		} catch (error) {
+			if (error instanceof DatabaseDuplicateKeyError) {
+				return {
+					error: new SessionAlreadyExistsError(),
+					success: false,
+				};
+			}
+
+			return this._handleError(error);
+		}
 	}
 
 	private _handleError(error: unknown): FailureResult<SessionBaseError> {
