f0aec1a0f6a7cae89f7c30d323705fc3b4b5fbfe
Tno <msa@moh-sa.dev>
2025-08-30 18:43:01 +0300

    feat: add mock for `Session Service`

---
+++ server/src/tests/mocks/session-service.mock.ts
@@ -0,0 +1,32 @@
+/* eslint-disable perfectionist/sort-objects */
+
+import { mock } from "node:test";
+
+import type { ISessionService } from "../../services/index.js";
+import type { FunctionMocksWithReset } from "../types/mocked.type.js";
+
+export function mockSessionService(): FunctionMocksWithReset<ISessionService> {
+	return {
+		create: mock.fn(),
+		getActiveByUserId: mock.fn(),
+		getByTokenIdAndUserId: mock.fn(),
+		revokeAllByUserId: mock.fn(),
+		revokeByTokenIdAndUserId: mock.fn(),
+		validate: mock.fn(),
+		reset() {
+			this.create.mock.resetCalls();
+			this.getActiveByUserId.mock.resetCalls();
+			this.getByTokenIdAndUserId.mock.resetCalls();
+			this.revokeAllByUserId.mock.resetCalls();
+			this.revokeByTokenIdAndUserId.mock.resetCalls();
+			this.validate.mock.resetCalls();
+
+			this.create.mock.restore();
+			this.getActiveByUserId.mock.restore();
+			this.getByTokenIdAndUserId.mock.restore();
+			this.revokeAllByUserId.mock.restore();
+			this.revokeByTokenIdAndUserId.mock.restore();
+			this.validate.mock.restore();
+		},
+	};
+}
