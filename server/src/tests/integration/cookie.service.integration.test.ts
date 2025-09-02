2fc07ace53060d45f01f7d93d2a6142232a6c845
Tno <msa@moh-sa.dev>
2025-09-02 19:38:50 +0300

    test: add integration test skeleton for `Cookie Service`

---
+++ server/src/tests/integration/cookie.service.integration.test.ts
@@ -0,0 +1,16 @@
+import { describe, suite } from "node:test";
+
+import { CookieService } from "../../services/index.js";
+
+suite(
+	"Cookie Service 〖 Integration Tests 〗",
+	{ skip: true, todo: "IMPLEMENT" },
+	() => {
+		const service = new CookieService(); // eslint-disable-line @typescript-eslint/no-unused-vars
+
+		// I AM TIRED BOSS :')
+		describe("set", () => {});
+		describe("get", () => {});
+		describe("delete", () => {});
+	},
+);
