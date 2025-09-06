5fd69d4f26e11e6f8c38a8b0fbb85865aa59700f
Tno <msa@moh-sa.dev>
2025-09-06 15:58:25 +0300

    fix: update `revokedAt` field in `session zod schema` to 1) be optional 2) default to null

--- server/src/schemas/session/session.schema.ts
+++ server/src/schemas/session/session.schema.ts
@@ -7,12 +7,15 @@ import {
 
 const baseSessionSchema = z.object({
 	expiresAt: z.date(),
-	revokedAt: z.date().nullable(),
+	revokedAt: z.date().nullable().default(null),
 	tokenId: jwtTokenValidator,
 	userId: objectIdValidator,
 });
 
-export const insertSessionSchema = baseSessionSchema;
+export const insertSessionSchema = baseSessionSchema.partial({
+	revokedAt: true,
+});
+
 export const selectSessionSchema = baseSessionSchema.extend({
 	createdAt: z.date(),
 	id: objectIdValidator,
