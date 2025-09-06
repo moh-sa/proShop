6f64638c11fcec91fba93d5bfa98cc49d03a7ffa
Tno <msa@moh-sa.dev>
2025-09-06 18:06:01 +0300

    test: add mock implementation for `create` method in user service and update reset functionality

--- server/src/tests/mocks/user-service.mock.ts
+++ server/src/tests/mocks/user-service.mock.ts
@@ -5,17 +5,20 @@ import type { FunctionMocksWithReset } from "../types/mocked.type.js";
 
 export function mockUserService(): FunctionMocksWithReset<IUserService> {
 	return {
+		create: mock.fn(),
 		delete: mock.fn(),
 		getAll: mock.fn(),
 		getByEmail: mock.fn(),
 		getById: mock.fn(),
 		reset() {
+			this.create.mock.resetCalls();
 			this.getAll.mock.resetCalls();
 			this.getById.mock.resetCalls();
 			this.getByEmail.mock.resetCalls();
 			this.updateById.mock.resetCalls();
 			this.delete.mock.resetCalls();
 
+			this.create.mock.restore();
 			this.getAll.mock.restore();
 			this.getById.mock.restore();
 			this.getByEmail.mock.restore();
