b730c57d64f6f842f6f208f296defb34b7aa4ca2
Tno <msa@moh-sa.dev>
2025-09-06 17:53:19 +0300

    feat: enhance `InternalError` class to accept optional details parameter

--- server/src/errors/internal.error.ts
+++ server/src/errors/internal.error.ts
@@ -2,7 +2,10 @@ import { ErrorType } from "../types/index.js";
 import { BaseError } from "./base.error.js";
 
 export class InternalError extends BaseError {
-	constructor(message: string = "Internal server error") {
-		super(message, ErrorType.INTERNAL, 500);
+	constructor(
+		message: string = "Internal server error",
+		details?: Record<string, unknown>,
+	) {
+		super(message, ErrorType.INTERNAL, 500, details);
 	}
 }
