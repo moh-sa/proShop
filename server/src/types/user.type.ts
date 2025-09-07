95f5902cd1aa8f1eb4558d32d7e4121d6371afc3
Tno <msa@moh-sa.dev>
2025-09-08 00:34:17 +0300

    feat: add `Safe Select User` and `Unsafe Select User` types

--- server/src/types/user.type.ts
+++ server/src/types/user.type.ts
@@ -5,3 +5,7 @@ import type { insertUserSchema, selectUserSchema } from "../schemas/index.js";
 export type InsertUser = z.infer<typeof insertUserSchema>;
 export type SelectUser = z.infer<typeof selectUserSchema>;
 export type UserSchema = SelectUser;
+
+// SAFE/UNSAFE user types
+export type SafeSelectUser = Omit<SelectUser, "password">;
+export type UnSafeSelectUser = SelectUser;
