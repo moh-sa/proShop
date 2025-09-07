4330b50b2db219fe21ec66f672e6a8496e43083b
Tno <msa@moh-sa.dev>
2025-09-08 01:45:35 +0300

    feat: initialized `Auth Manager`

---
+++ server/src/managers/auth.manager.ts
@@ -0,0 +1,41 @@
+import type {
+	IJwtService,
+	IPasswordService,
+	ISessionService,
+	IUserService,
+} from "../services/index.js";
+import type { Result } from "../types/index.js";
+
+import {
+	JwtService,
+	PasswordService,
+	SessionService,
+	UserService,
+} from "../services/index.js";
+
+// helpers types
+type AuthResult<T> = Result<T>;
+type Params<T extends keyof IAuthManager> = Parameters<IAuthManager[T]>[0];
+type Return<T extends keyof IAuthManager> = ReturnType<IAuthManager[T]>;
+
+// interfaces
+interface IAuthManager {}
+
+export class AuthManager implements IAuthManager {
+	private readonly _jwt: IJwtService;
+	private readonly _password: IPasswordService;
+	private readonly _session: ISessionService;
+	private readonly _user: IUserService;
+
+	constructor(
+		jwt: IJwtService = new JwtService(),
+		password: IPasswordService = new PasswordService(),
+		session: ISessionService = new SessionService(),
+		user: IUserService = new UserService(),
+	) {
+		this._jwt = jwt;
+		this._password = password;
+		this._session = session;
+		this._user = user;
+	}
+}
