9ad03fd7536329df01a3fa55ad32b91f610b9d9e
Tno <msa@moh-sa.dev>
2025-09-07 23:57:37 +0300

    fix: add missing default values to the constructor of `DatabaseBaseError` class

--- server/src/errors/database/database-base.error.ts
+++ server/src/errors/database/database-base.error.ts
@@ -1,3 +1,13 @@
+import { ErrorType } from "../../types/error.type.js";
 import { BaseError } from "../base.error.js";
 
-export class DatabaseBaseError extends BaseError {}
+export class DatabaseBaseError extends BaseError {
+	constructor(
+		message: string,
+		type: ErrorType = ErrorType.INTERNAL,
+		statusCode: number = 500,
+		details?: Record<string, unknown>,
+	) {
+		super(message, type, statusCode, details);
+	}
+}
