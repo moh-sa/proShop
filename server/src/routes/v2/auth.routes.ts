b2662b02c56168c0b3c1c541460e73fc640d54eb
Tno <msa@moh-sa.dev>
2025-09-18 13:52:59 +0300

    feat: add `POST /v2/auth/signup`route

--- server/src/routes/v2/auth.routes.ts
+++ server/src/routes/v2/auth.routes.ts
@@ -1,11 +1,13 @@
 import express from "express";
 
 import { Auth2Controller } from "../../controllers/auth2.controller.js";
+import { authLimiter } from "../../services/index.js";
 
 const router = express.Router();
-const controller = new Auth2Controller(); // eslint-disable-line @typescript-eslint/no-unused-vars
+const controller = new Auth2Controller();
 
 //============= 🔓 PUBLIC ROUTES =============
+router.route("/signup").post(authLimiter, controller.signUp);
 
 //============= 🔒 PROTECTED ROUTES =============
 const protectedRouter = express.Router();
