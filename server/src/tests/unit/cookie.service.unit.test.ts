25d9b3ea07a98d80df2808ea93e9d83b9d72ff4d
Tno <msa@moh-sa.dev>
2025-09-21 22:13:01 +0300

    test: add unit tests for `get` in `Cookie Service`

--- server/src/tests/unit/cookie.service.unit.test.ts
+++ server/src/tests/unit/cookie.service.unit.test.ts
@@ -1,11 +1,13 @@
 import assert from "node:assert";
 import { describe, it, suite } from "node:test";
+import { z } from "zod";
 
 import type { CookieItemOptions } from "../../types/index.js";
 
 import { DEFAULT_COOKIE_CONFIG } from "../../config/index.js";
 import { CookieName } from "../../constants/index.js";
 import {
+	CookieNotFoundError,
 	CookieOperationError,
 	CookieSerializationError,
 	CookieValidationError,
@@ -76,7 +78,6 @@ suite("Cookie Service〖 Unit Tests 〗", () => {
 
 			// Assert
 			assert.strictEqual(result.success, false);
-			console.log("😊😊😊😊 RESULT 1: ", result);
 			assert(result.error instanceof CookieValidationError);
 		});
 
@@ -92,7 +93,6 @@ suite("Cookie Service〖 Unit Tests 〗", () => {
 
 			// Assert
 			assert.strictEqual(result.success, false);
-			console.log("😊😊😊😊 RESULT 2: ", result);
 			assert(result.error instanceof CookieValidationError);
 		});
 
@@ -147,6 +147,123 @@ suite("Cookie Service〖 Unit Tests 〗", () => {
 			assert(result.error instanceof CookieOperationError);
 		});
 	});
-	describe("get", () => {});
+
+	describe("get", () => {
+		it("should get cookie without schema", () => {
+			// Arrange
+			const name = CookieName.REFRESH_TOKEN;
+			const data = { theme: "dark" };
+			const { req } = createMockExpressContext();
+			req.signedCookies = { [name]: JSON.stringify(data) };
+
+			// Act
+			const result = service.get<typeof data>({ name, request: req });
+
+			// Assert
+			assert.strictEqual(result.success, true);
+			assert.deepStrictEqual(result.data, data);
+		});
+
+		it("should get cookie and validate with schema", () => {
+			// Arrange
+			const name = CookieName.ACCESS_TOKEN;
+			const data = { age: 30, id: "u1" };
+			const { req } = createMockExpressContext();
+			req.signedCookies = { [name]: JSON.stringify(data) };
+
+			const schema = z.object({ age: z.number(), id: z.string() });
+
+			// Act
+			const result = service.get<typeof data>({
+				name,
+				request: req,
+				schema,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, true);
+			assert.deepStrictEqual(result.data, data);
+		});
+
+		it("should fail when cookie name is empty", () => {
+			// Arrange
+			const { req } = createMockExpressContext();
+
+			// Act
+			const result = service.get({ name: "" as any, request: req });
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert(result.error instanceof CookieValidationError);
+		});
+
+		it("should fail when request is invalid", () => {
+			// Arrange
+			const invalidReq = {} as any;
+
+			// Act
+			const result = service.get({
+				name: CookieName.ACCESS_TOKEN,
+				request: invalidReq,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert(result.error instanceof CookieValidationError);
+		});
+
+		it("should fail when cookie is not found", () => {
+			// Arrange
+			const { req } = createMockExpressContext();
+			req.signedCookies = {};
+
+			// Act
+			const result = service.get({
+				name: CookieName.ACCESS_TOKEN,
+				request: req,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert(result.error instanceof CookieNotFoundError);
+		});
+
+		it("should fail when parsing JSON throws", () => {
+			// Arrange
+			const name = CookieName.ACCESS_TOKEN;
+			const { req } = createMockExpressContext();
+			req.signedCookies = { [name]: "{" };
+
+			// Act
+			const result = service.get({ name, request: req });
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert(result.error instanceof CookieSerializationError);
+		});
+
+		it("should fail when schema validation fails", () => {
+			// Arrange
+			const name = CookieName.REFRESH_TOKEN;
+			const data = { age: "30", id: "u1" };
+			const { req } = createMockExpressContext();
+			req.signedCookies = { [name]: JSON.stringify(data) };
+
+			const schema = z.object({ age: z.number(), id: z.string() });
+
+			// Act
+			const result = service.get<typeof data>({
+				name,
+				request: req,
+				// @ts-expect-error - test case
+				schema,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert(result.error instanceof CookieValidationError);
+		});
+	});
+
 	describe("delete", () => {});
 });
