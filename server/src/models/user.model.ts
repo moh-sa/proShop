8c15947e8098359e691f5c96a437e484127efb37
Tno <msa@moh-sa.dev>
2025-09-22 14:20:45 +0300

    refactor: remove unneeded pre save hook in `User Model`

--- server/src/models/user.model.ts
+++ server/src/models/user.model.ts
@@ -8,7 +8,6 @@ import {
 	MIN_NAME_LENGTH,
 	MIN_PASSWORD_LENGTH,
 } from "../constants/index.js";
-import { PasswordService } from "../services/index.js";
 
 const userSchema = new Schema<UserSchema>(
 	{
@@ -55,27 +54,6 @@ const userSchema = new Schema<UserSchema>(
 		timestamps: true,
 	},
 );
-
-// TODO: remove after implementing auth v2
-userSchema.pre("save", async function (next) {
-	if (!this.isModified("password")) {
-		return next();
-	}
-
-	const pswService = new PasswordService();
-	try {
-		const passwordHashingResult = await pswService.hash({
-			password: this.password,
-		});
-		if (!passwordHashingResult.success) {
-			return next(passwordHashingResult.error);
-		}
-		this.password = passwordHashingResult.data;
-		next();
-	} catch (error) {
-		return next(error as Error);
-	}
-});
 
 const User = model<UserSchema>("User", userSchema);
 
