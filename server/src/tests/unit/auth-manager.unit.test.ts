855a4886d2ab057b1238c53db8fb501567e25dfd
Tno <msa@moh-sa.dev>
2025-09-24 18:06:37 +0300

    test: add unit tests for the `refreshAccessToken` method in `Auth Manager`

--- server/src/tests/unit/auth-manager.unit.test.ts
+++ server/src/tests/unit/auth-manager.unit.test.ts
@@ -1,13 +1,17 @@
 import assert from "node:assert";
 import { beforeEach, describe, it, suite } from "node:test";
 
+import type { TokenResult } from "../../types/index.js";
+
 import { ValidationError } from "../../errors/index.js";
 import { AuthManager } from "../../managers/auth.manager.js";
 import { TokenType } from "../../types/index.js";
 import {
 	generateMockJwt,
+	generateMockSelectSession,
 	generateMockSelectSessions,
 	generateMockTokenDecoded,
+	generateMockTokenWithData,
 	mockJwtService,
 	mockPasswordService,
 	mockSessionService,
@@ -133,7 +137,153 @@ suite("Auth Manager 〖 Unit Tests 〗", () => {
 			assert.strictEqual(result.error, error);
 		});
 	});
-	describe("refreshAccessToken", () => {});
+
+	describe("refreshAccessToken", () => {
+		it("should return success with new access token when refresh token and session are valid", async () => {
+			// Arrange
+			const mockRefreshToken = generateMockJwt(TokenType.REFRESH);
+			const mockDecodedToken = generateMockTokenDecoded(TokenType.REFRESH);
+
+			const mockAccessData = generateMockTokenWithData(TokenType.ACCESS);
+			const access: TokenResult = {
+				expiresAt: new Date(mockAccessData.exp * 1000),
+				token: mockAccessData.token,
+				tokenId: mockAccessData.tokenId,
+			};
+
+			mockJwt.verify.mock.mockImplementation(() => ({
+				data: mockDecodedToken,
+				success: true,
+			}));
+			mockSession.validate.mock.mockImplementation(async () => ({
+				data: generateMockSelectSession(),
+				success: true,
+			}));
+			mockJwt.generateAccessToken.mock.mockImplementation(() => ({
+				data: access,
+				success: true,
+			}));
+
+			// Act
+			const result = await manager.refreshAccessToken({
+				refreshToken: mockRefreshToken,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, true);
+			assert.strictEqual(result.data.token, access.token);
+			assert.strictEqual(result.data.expiresAt, access.expiresAt);
+
+			assert.strictEqual(mockSession.validate.mock.callCount(), 1);
+			assert.strictEqual(
+				mockSession.validate.mock.calls[0].arguments[0].tokenId,
+				mockDecodedToken.tokenId,
+			);
+			assert.strictEqual(
+				mockSession.validate.mock.calls[0].arguments[0].userId,
+				mockDecodedToken.userId,
+			);
+
+			assert.strictEqual(mockJwt.verify.mock.callCount(), 1);
+			assert.strictEqual(mockJwt.generateAccessToken.mock.callCount(), 1);
+			assert.strictEqual(
+				mockJwt.generateAccessToken.mock.calls[0].arguments[0].userId,
+				mockDecodedToken.userId,
+			);
+		});
+
+		it("should return ValidationError when refreshToken is missing", async () => {
+			// Arrange
+			const emptyRefreshToken = "";
+
+			// Act
+			const result = await manager.refreshAccessToken({
+				refreshToken: emptyRefreshToken,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert.ok(result.error instanceof ValidationError);
+
+			assert.strictEqual(mockJwt.verify.mock.callCount(), 0);
+		});
+
+		it("should bubble jwt.verify error", async () => {
+			// Arrange
+			const invalidToken = "invalid-jwt";
+			const error = new ValidationError("Invalid token");
+
+			mockJwt.verify.mock.mockImplementation(() => ({
+				error,
+				success: false,
+			}));
+
+			// Act
+			const result = await manager.refreshAccessToken({
+				refreshToken: invalidToken,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert.strictEqual(result.error, error);
+		});
+
+		it("should bubble session.validate error", async () => {
+			// Arrange
+			const mockRefreshToken = generateMockJwt(TokenType.REFRESH);
+			const mockDecodedToken = generateMockTokenDecoded(TokenType.REFRESH);
+
+			const error = new ValidationError("session");
+
+			mockJwt.verify.mock.mockImplementation(() => ({
+				data: mockDecodedToken,
+				success: true,
+			}));
+			mockSession.validate.mock.mockImplementation(async () => ({
+				error,
+				success: false,
+			}));
+
+			// Act
+			const result = await manager.refreshAccessToken({
+				refreshToken: mockRefreshToken,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert.strictEqual(result.error, error);
+		});
+
+		it("should bubble access token generation error", async () => {
+			// Arrange
+			const mockRefreshToken = generateMockJwt(TokenType.REFRESH);
+			const mockDecodedToken = generateMockTokenDecoded(TokenType.REFRESH);
+
+			const error = new ValidationError("gen");
+
+			mockJwt.verify.mock.mockImplementation(() => ({
+				data: mockDecodedToken,
+				success: true,
+			}));
+			mockSession.validate.mock.mockImplementation(async () => ({
+				data: generateMockSelectSession(),
+				success: true,
+			}));
+			mockJwt.generateAccessToken.mock.mockImplementation(() => ({
+				error,
+				success: false,
+			}));
+
+			// Act
+			const result = await manager.refreshAccessToken({
+				refreshToken: mockRefreshToken,
+			});
+
+			// Assert
+			assert.strictEqual(result.success, false);
+			assert.strictEqual(result.error, error);
+		});
+	});
 	describe("revokeSession", () => {});
 	describe("signIn", () => {});
 	describe("signOut", () => {});
