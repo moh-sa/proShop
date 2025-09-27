0b03962cb2b9046a7a6834c47004bf88d95d34f5
Tno <msa@moh-sa.dev>
2025-09-28 01:35:29 +0300

    feat: add `createStrictSuccessResponseObject` and `sendStrictSuccessResponse` utilities

--- server/src/utils/success-response.util.ts
+++ server/src/utils/success-response.util.ts
@@ -1,7 +1,7 @@
 import type { Response } from "express";
 
 import type { HTTP_STATUS } from "../constants/index.js";
-import type { SuccessResponse } from "../types/index.js";
+import type { StrictSuccessResponse, SuccessResponse } from "../types/index.js";
 
 export function createSuccessResponseObject<D, M = undefined>({
 	data,
@@ -27,5 +27,40 @@ export function sendSuccessResponse<D, M = undefined>({
 	statusCode: HTTP_STATUS;
 }): void {
 	const response = createSuccessResponseObject({ data, meta });
+	responseContext.status(statusCode).json(response);
+}
+
+//
+export function createStrictSuccessResponseObject<D, M>(
+	data: D,
+	meta?: M,
+): StrictSuccessResponse<{ data: D }>;
+export function createStrictSuccessResponseObject<D, M>(
+	data: D,
+	meta: M,
+): StrictSuccessResponse<{ data: D; meta: M }>;
+export function createStrictSuccessResponseObject<D, M>(data: D, meta: M) {
+	if (data && meta) {
+		return { data, meta, success: true };
+	}
+	if (data) {
+		return { data, success: true };
+	}
+
+	if (meta) {
+		return { meta, success: true };
+	}
+}
+
+export function sendStrictSuccessResponse<D, M = undefined>({
+	data,
+	meta,
+	responseContext,
+	statusCode,
+}: StrictSuccessResponse<{ data: D; meta: M }> & {
+	responseContext: Response;
+	statusCode: HTTP_STATUS;
+}): void {
+	const response = createStrictSuccessResponseObject({ data, meta });
 	responseContext.status(statusCode).json(response);
 }
