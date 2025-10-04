7b59690106f703b419f86df3d312047eec664cf6
Tno <msa@moh-sa.dev>
2025-10-04 21:02:29 +0300

    refactor: remove fields related to saving images on disk in `insertImageSchema`

--- server/src/schemas/product/image.schema.ts
+++ server/src/schemas/product/image.schema.ts
@@ -1,18 +1,16 @@
-import { Readable } from "node:stream";
 import { z } from "zod";
 
 import { IMAGE_SIZE_LIMIT, IMAGE_TYPE_LIMIT } from "../../constants/index.js";
 
 export const insertImageSchema = z.object({
-	buffer: z
-		.unknown()
-		.refine((val): val is Buffer => Buffer.isBuffer(val), {
-			message: "Invalid buffer",
-		}),
-	destination: z.string().min(1),
+	buffer: z.unknown().refine((val): val is Buffer => Buffer.isBuffer(val), {
+		message: "Invalid buffer",
+	}),
+
 	encoding: z.string().min(1),
+
 	fieldname: z.string().min(1),
-	filename: z.string().min(1),
+
 	mimetype: z.string().refine((val) => IMAGE_TYPE_LIMIT.includes(val), {
 		message: `Invalid image type. Allowed types: ${IMAGE_TYPE_LIMIT.map((val) =>
 			val.replace("image/", ""),
@@ -20,12 +18,11 @@ export const insertImageSchema = z.object({
 	}),
 
 	originalname: z.string().min(1),
-	path: z.string().min(1),
+
 	size: z
 		.number()
 		.positive()
 		.max(IMAGE_SIZE_LIMIT, { message: "Image size should not exceed 5MB" }),
-	stream: z.instanceof(Readable),
 });
 
 export const selectImageSchema = z
