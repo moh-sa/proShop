08c705e8b38f09da20adbb7d994dea44c45d366e
Tno <msa@moh-sa.dev>
2025-10-12 02:39:39 +0300

    feat: initialized `Paginator` utility class

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -5,6 +5,7 @@ export * from "./handle-database-error.util.js";
 export * from "./is-exist.util.js";
 export * from "./jwt-generate-token.util.js";
 export * from "./jwt-verify-token.util.js";
+export * from "./paginator.util.js";
 export * from "./remove-empty-fields.util.js";
 export * from "./remove-object-fields.js";
 export * from "./success-response.util.js";
