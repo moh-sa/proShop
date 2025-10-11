289e69455787efbd8a7c4f41b69057d62ac3db53
Tno <msa@moh-sa.dev>
2025-10-12 02:34:03 +0300

    feat: add pagination constants for default and maximum page sizes

--- server/src/constants/index.ts
+++ server/src/constants/index.ts
@@ -1,6 +1,7 @@
 export * from "./cookie.constants.js";
 export * from "./error-type.constants.js";
 export * from "./http-status.constants.js";
+export * from "./pagination.constants.js";
 export * from "./password.constants.js";
 export * from "./product.constants.js";
 export * from "./storage.constants.js";
