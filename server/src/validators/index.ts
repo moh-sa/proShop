333f2205f5f9097773c9cce2da8df9529a99131a
Tno <msa@moh-sa.dev>
2025-10-12 21:42:23 +0300

    feat: add pagination parameters validator to validate and transform the `query` and `sort` using the `Paginator` static methods

--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -3,5 +3,6 @@ export * from "./email.validator.js";
 export * from "./jwt-token.validator.js";
 export * from "./non-empty-string.validator.js";
 export * from "./object-id.validator.js";
+export * from "./pagination.validator.js";
 export * from "./password.validator.js";
 export * from "./uuid.validator.js";
