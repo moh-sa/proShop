629f8d0c27ba901875d9d29f2760e70a2095c82d
Tno <msa@moh-sa.dev>
2025-10-16 15:21:07 +0300

    feat: add review pagination params types for product and user IDs

--- server/src/types/review.type.ts
+++ server/src/types/review.type.ts
@@ -4,7 +4,17 @@ import type {
 	insertReviewSchema,
 	selectReviewSchema,
 } from "../schemas/index.js";
+import type { PaginationParamsString } from "./pagination.type.js";
 
 export type InsertReview = z.infer<typeof insertReviewSchema>;
 export type ReviewSchema = SelectReview;
 export type SelectReview = z.infer<typeof selectReviewSchema>;
+
+// Pagination
+export type ReviewPaginationParamsByProductId = PaginationParamsString & {
+	productId: string;
+};
+
+export type ReviewPaginationParamsByUserId = PaginationParamsString & {
+	userId: string;
+};
