95813a24526b5a440cdfb9d006cc5b6b6f397bac
Tno <msa@moh-sa.dev>
2025-10-16 14:58:40 +0300

    feat: add 'SessionPaginationParams' type

--- server/src/types/session.type.ts
+++ server/src/types/session.type.ts
@@ -4,7 +4,13 @@ import type {
 	insertSessionSchema,
 	selectSessionSchema,
 } from "../schemas/index.js";
+import type { PaginationParamsString } from "./pagination.type.js";
 
 export type InsertSession = z.infer<typeof insertSessionSchema>;
 export type SelectSession = z.infer<typeof selectSessionSchema>;
 export type SessionSchema = SelectSession;
+
+// Pagination
+export type SessionPaginationParams = PaginationParamsString & {
+	userId: string;
+};
