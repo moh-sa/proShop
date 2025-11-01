7eb4bb4b76693cda9a7df25bb63c611660f876eb
Tno <msa@moh-sa.dev>
2025-11-01 20:45:44 +0200

    refactor: update Express Locals to use SafeSelectUser and add userId field. Removed review and token fields

--- server/src/types/express.d.ts
+++ server/src/types/express.d.ts
@@ -1,18 +1,10 @@
-import type { Types } from "mongoose";
-
-import type { SelectReview } from "./review.type.js";
-import type { SelectUser } from "./user.type.js";
+import type { SafeSelectUser } from "./user.type.js";
 
 declare global {
 	namespace Express {
 		interface Locals {
-			review: SelectReview;
-			token: {
-				_id: Types.ObjectId;
-				exp: number;
-				iat: number;
-			};
-			user: SelectUser;
+			user?: SafeSelectUser;
+			userId?: string;
 		}
 	}
 }
