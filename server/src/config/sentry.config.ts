bb512e368c08291c47fccf73259a37d4732fa126
Tno <msa@moh-sa.dev>
2025-11-03 22:39:09 +0200

    refactor: remove profiling integration and sample rates from Sentry configuration

--- server/src/config/sentry.config.ts
+++ server/src/config/sentry.config.ts
@@ -1,14 +1,8 @@
 import * as Sentry from "@sentry/node";
-import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 import { env } from "./env.js";
 
 Sentry.init({
 	dsn: env.SENTRY_DNS,
-	integrations: (integrations) => [
-		nodeProfilingIntegration(),
-		...integrations.filter((integration) => integration.name !== "Mongoose"),
-	],
-	profilesSampleRate: 1.0,
-	tracesSampleRate: 1.0,
+	environment: env.NODE_ENV,
 });
