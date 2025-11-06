df03ab8746dd2af665e8f5ce700045ecce9ab326
Tno <msa@moh-sa.dev>
2025-11-06 20:05:45 +0200

    feat: add async context utility for request-scoped logging

---
+++ server/src/utils/async-context.util.ts
@@ -0,0 +1,22 @@
+import type { Logger } from "pino";
+
+import { AsyncLocalStorage } from "node:async_hooks";
+
+interface AsyncContext {
+	logger: Logger;
+}
+
+/**
+ * AsyncLocalStorage instance for storing request-scoped data.
+ *
+ * This should only be used in middlewares.
+ * @example
+ * ```
+ * // in middleware
+ * asyncContext.run(
+ *   { logger: logger.child({...}) },
+ *   () => next(),
+ * );
+ * ```
+ */
+export const asyncContext = new AsyncLocalStorage<AsyncContext>();
--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -1,3 +1,4 @@
+export * from "./async-context.util.js";
 export * from "./async-handler.util.js";
 export * from "./error-response.util.js";
 export * from "./format-zod-errors.util.js";
