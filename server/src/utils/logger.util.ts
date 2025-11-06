019d892d76726b60fc3a48f2173efa3aca98972b
Tno <msa@moh-sa.dev>
2025-11-06 20:06:25 +0200

    docs: add doc to `logger` util function

--- server/src/utils/logger.util.ts
+++ server/src/utils/logger.util.ts
@@ -2,4 +2,16 @@ import pino from "pino";
 
 import { loggerConfig } from "../config/index.js";
 
+/**
+ * Root logger instance.
+ *
+ * **When to use:**
+ * - App startup/shutdown
+ * - Middleware that init request-scoped context
+ * - Code outside of request flow
+ * @example
+ * ```
+ * logger.info({...}, "message");
+ * ```
+ */
 export const logger = pino(loggerConfig);
