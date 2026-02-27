9eb208a21b8f54738a4c7a60c5650314e58292cc
Tno <msa@moh-sa.dev>
2026-02-27 22:23:25 +0300

    refactor: remove Auth Controller V1

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,5 +1,4 @@
 export * from "./auth2.controller.js";
-export * from "./auth.controller.js";
 export * from "./order.controller.js";
 export * from "./product.controller.js";
 export * from "./review.controller.js";
--- server/src/routes/v1/index.ts
+++ server/src/routes/v1/index.ts
@@ -1,6 +1,5 @@
 import { Router } from "express";
 
-import authRoutes from "./auth.routes.js";
 import orderRoutes from "./order.routes.js";
 import productRoutes from "./product.routes.js";
 import reviewRoutes from "./reviews.routes.js";
@@ -13,7 +12,6 @@ router.use("/products", productRoutes);
 router.use("/users", userRoutes);
 router.use("/orders", orderRoutes);
 router.use("/reviews", reviewRoutes);
-router.use("/auth", authRoutes);
 router.use("/webhooks", webhooksRoutes);
 
 export default router;
