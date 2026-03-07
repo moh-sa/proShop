16d0e0f3b438a215580b4924b793ca6213363ac7
Tno <msa@moh-sa.dev>
2026-03-07 20:41:51 +0300

    fix: change `paidAt` field to use date coercion in payment schema

--- server/src/schemas/order/order.schema.ts
+++ server/src/schemas/order/order.schema.ts
@@ -15,7 +15,7 @@ export const orderStatusSchema = z.enum([
 
 export const paymentSchema = z.object({
 	id: z.string().min(1, { message: "payment ID is required." }),
-	paidAt: z.date(),
+	paidAt: z.coerce.date(),
 	provider: paymentProviderSchema,
 	sessionURL: z
 		.string()
