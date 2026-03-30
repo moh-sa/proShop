6336827fdb85d0ce52ddaae0a1f06d337c7fc0da
Tno <msa@moh-sa.dev>
2026-03-30 16:57:45 +0300

    feat: enhance order types with pagination filters, sorting, and user-specific parameters

--- server/src/types/order.type.ts
+++ server/src/types/order.type.ts
@@ -6,6 +6,8 @@ import type {
 	markAsBaseParamsSchema,
 	markAsCancelledParamsSchema,
 	markAsProcessingParamsSchema,
+	orderPaginationFiltersSchema,
+	orderPaginationSortSchema,
 	orderStatusSchema,
 	selectOrderSchema,
 } from "../schemas/index.js";
@@ -13,29 +15,52 @@ import type {
 	insertOrderItemSchema,
 	selectOrderItemSchema,
 } from "../schemas/order/order-item.schema.js";
-import type { PaginationParamsString } from "./pagination.type.js";
+import type {
+	PaginationFilter,
+	PaginationParams,
+	PaginationParamsStringified,
+	PaginationSelect,
+} from "./pagination.type.js";
+import type { Stringify } from "./stringify.type.js";
 
-export type AllOrdersResponse = z.infer<typeof allOrdersResponseSchema>;
 export type InsertOrder = z.infer<typeof insertOrderSchema>;
 export type InsertOrderItem = z.infer<typeof insertOrderItemSchema>;
 export type OrderSchema = SelectOrder;
 export type OrderStatus = z.infer<typeof orderStatusSchema>;
-
 export type SelectOrder = z.infer<typeof selectOrderSchema>;
 export type SelectOrderItem = z.infer<typeof selectOrderItemSchema>;
 
 // Pagination
-export type OrderPaginationParams = PaginationParamsString & {
+export type OrderFilter = PaginationFilter<
+	z.infer<typeof orderPaginationFiltersSchema>
+>;
+
+export type OrderSelect = PaginationSelect<SelectOrder>;
+
+export type OrderSort = z.infer<typeof orderPaginationSortSchema>;
+
+// Method Params
+export type GetAllOrdersByUserIdControllerParams =
+	GetAllOrdersControllerParams & {
+		userId: string;
+	};
+
+export type GetAllOrdersControllerParams = PaginationParamsStringified & {
+	sort?: string;
 	status?: string;
-	user?: string;
 };
 
-/** Response from creating an order with checkout session */
-export type CreateOrderResponse = {
-	order: SelectOrder;
-	session: {
-		url: string;
-	};
+export type GetAllOrdersManagerParams = GetAllOrdersServiceParams;
+
+export type GetAllOrdersRepositoryParams = PaginationParams & {
+	filters?: OrderFilter;
+	select?: OrderSelect;
+	sort?: OrderSort;
+};
+
+export type GetAllOrdersServiceParams = PaginationParamsStringified & {
+	filters?: Stringify<OrderFilter>;
+	sort?: string;
 };
 
 export type MarkAsBaseParams = z.infer<typeof markAsBaseParamsSchema>;
@@ -45,3 +70,14 @@ export type MarkAsCancelledParams = z.infer<typeof markAsCancelledParamsSchema>;
 export type MarkAsProcessingParams = z.infer<
 	typeof markAsProcessingParamsSchema
 >;
+
+// Method Response
+/** Response from creating an order with checkout session */
+export type AllOrdersResponse = z.infer<typeof allOrdersResponseSchema>;
+
+export type CreateOrderResponse = {
+	order: SelectOrder;
+	session: {
+		url: string;
+	};
+};
