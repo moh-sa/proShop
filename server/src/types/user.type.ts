7722ea310daf531da8d7c7b0d25f751c0fcd96d4
Tno <msa@moh-sa.dev>
2026-03-31 09:38:42 +0300

    feat: user pagination filters type

--- server/src/types/user.type.ts
+++ server/src/types/user.type.ts
@@ -1,13 +1,16 @@
 import type { z } from "zod";
 
-import User from "../models/user.model.js";
-import type { insertUserSchema, selectUserSchema } from "../schemas/index.js";
-import type { PaginationParamsString } from "./pagination.type.js";
-
-export type UserDocument = ReturnType<(typeof User)["hydrate"]>;
+import type User from "../models/user.model.js";
+import type {
+	insertUserSchema,
+	selectUserSchema,
+	userPaginationFiltersSchema,
+} from "../schemas/index.js";
+import type { PaginationFilter } from "./pagination.type.js";
 
 export type InsertUser = z.infer<typeof insertUserSchema>;
 export type SelectUser = z.infer<typeof selectUserSchema>;
+export type UserDocument = ReturnType<(typeof User)["hydrate"]>;
 export type UserSchema = SelectUser;
 
 // SAFE/UNSAFE user types
@@ -15,11 +18,6 @@ export type SafeSelectUser = Omit<SelectUser, "password">;
 export type UnSafeSelectUser = SelectUser;
 
 // Pagination
-export type UserPaginationParams = Omit<
-	PaginationParamsString,
-	"pipeline" | "query"
-> & {
-	email?: string;
-	isAdmin?: string;
-	name?: string;
-};
+export type UserFilter = PaginationFilter<
+	z.infer<typeof userPaginationFiltersSchema>
+>;
