f4f552513a9388f4436fecb6e432f443e591ba14
Tno <msa@moh-sa.dev>
2026-04-04 12:13:26 +0300

    feat: add `reviewPaginationFiltersSchema` for validating review pagination filters by product and user IDs

---
+++ server/src/schemas/review/review-pagination.schema.ts
@@ -0,0 +1,20 @@
+import { isValidObjectId } from "mongoose";
+import z from "zod";
+
+// Filters
+const baseFiltersSchema = z.object({
+	productId: z
+		.string()
+		.trim()
+		.refine((v) => isValidObjectId(v), {
+			message: "Invalid product ID",
+		}),
+	userId: z
+		.string()
+		.trim()
+		.refine((v) => isValidObjectId(v), {
+			message: "Invalid user ID",
+		}),
+});
+
+export const reviewPaginationFiltersSchema = baseFiltersSchema.partial();
--- server/src/types/review.type.ts
+++ server/src/types/review.type.ts
@@ -4,17 +4,14 @@ import type {
 	insertReviewSchema,
 	selectReviewSchema,
 } from "../schemas/index.js";
-import type { PaginationParamsString } from "./pagination.type.js";
+import type { reviewPaginationFiltersSchema } from "../schemas/review/review-pagination.schema.js";
+import type { PaginationFilter } from "./pagination.type.js";
 
 export type InsertReview = z.infer<typeof insertReviewSchema>;
 export type ReviewSchema = SelectReview;
 export type SelectReview = z.infer<typeof selectReviewSchema>;
 
 // Pagination
-export type ReviewPaginationParamsByProductId = PaginationParamsString & {
-	productId: string;
-};
-
-export type ReviewPaginationParamsByUserId = PaginationParamsString & {
-	userId: string;
-};
+export type ReviewFilter = PaginationFilter<
+	z.infer<typeof reviewPaginationFiltersSchema>
+>;
