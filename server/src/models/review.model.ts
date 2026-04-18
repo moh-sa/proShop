024ccdb2392b23f67475446ab865bb845985fc21
Tno <msa@moh-sa.dev>
2026-04-18 18:55:53 +0300

    feat: add `product rating` index to review schema

--- server/src/models/review.model.ts
+++ server/src/models/review.model.ts
@@ -43,8 +43,10 @@ const reviewSchema = new Schema<ReviewSchema>(
 	},
 );
 
+// Indexes
 // Compound index to ensure ONE review per user per product
 reviewSchema.index({ product: 1, "user.id": 1 }, { unique: true });
+reviewSchema.index({ product: 1, rating: -1 });
 
 // Update product 'rating' and 'numReviews' after review is saved or updated
 async function updateProductRating(productId: Types.ObjectId) {
