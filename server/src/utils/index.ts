62318b757236b2dd170d797ec5faf3eab4f416e1
Tno <msa@moh-sa.dev>
2026-04-19 12:31:31 +0300

    refactor: remove deprecated `generateJwtToken` utility

--- server/src/utils/index.ts
+++ server/src/utils/index.ts
@@ -6,7 +6,6 @@ export * from "./format-zod-errors.util.js";
 export * from "./frontend-url-builder.util.js";
 export * from "./get-currency-factor.util.js";
 export * from "./handle-database-error.util.js";
-export * from "./jwt-generate-token.util.js";
 export * from "./logger.util.js";
 export * from "./paginator.util.js";
 export * from "./remove-object-fields.js";
