d60cf622d53df4c055b580b26e68964c74898be8
Tno <msa@moh-sa.dev>
2026-04-19 12:46:56 +0300

    refactor: remove unused export of `PartialBy` type

--- server/src/types/index.ts
+++ server/src/types/index.ts
@@ -12,7 +12,6 @@ export * from "./method-params.type.js";
 export * from "./method-return.type.js";
 export * from "./order.type.js";
 export * from "./pagination.type.js";
-export * from "./partial-by.type.js";
 export * from "./payment.types.js";
 export * from "./product.type.js";
 export * from "./rate-limit.type.js";
