15b3cab7205a54a5e06c2f559378ddcee07095a5
Tno <msa@moh-sa.dev>
2026-04-19 12:33:16 +0300

    refactor: remove unused `stringToStrictBooleanValidator`

--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -3,6 +3,5 @@ export * from "./jwt-token.validator.js";
 export * from "./non-empty-string.validator.js";
 export * from "./object-id.validator.js";
 export * from "./password.validator.js";
-export * from "./string-to-boolean.validator.js";
 export * from "./url.validator.js";
 export * from "./uuid.validator.js";
