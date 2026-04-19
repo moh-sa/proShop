681d42be8806ba3fcb7213fd97e77047f3cd703f
Tno <msa@moh-sa.dev>
2026-04-19 12:33:04 +0300

    refactor: remove deprecated `paginationParamsValidator`

--- server/src/validators/index.ts
+++ server/src/validators/index.ts
@@ -2,7 +2,6 @@ export * from "./email.validator.js";
 export * from "./jwt-token.validator.js";
 export * from "./non-empty-string.validator.js";
 export * from "./object-id.validator.js";
-export * from "./pagination.validator.js";
 export * from "./password.validator.js";
 export * from "./string-to-boolean.validator.js";
 export * from "./url.validator.js";
--- server/src/validators/pagination.validator.ts
+++
@@ -1,3 +0,0 @@
-import { paginationParamsSchema } from "../schemas/pagination/pagination.schema.js";
-/** @deprecated Use `paginationParamsSchema` instead */
-export const paginationParamsValidator = paginationParamsSchema;
