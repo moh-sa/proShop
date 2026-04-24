49e81f0ac9fc351a876e93d79b081bcb1fd5d529
Tno <msa@moh-sa.dev>
2026-04-24 14:29:44 +0300

    refactor: remove 'domain' from cookie default config

--- server/src/config/cookie.config.ts
+++ server/src/config/cookie.config.ts
@@ -2,7 +2,6 @@ import type { CookieConfig } from "../types/index.js";
 import { env } from "./env.js";
 
 export const DEFAULT_COOKIE_CONFIG: CookieConfig = {
-	domain: env.NODE_ENV === "production" ? env.CLIENT_URL : "localhost",
 	encode: encodeURIComponent,
 	httpOnly: true,
 	path: "/",
