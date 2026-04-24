90569e4cfade787c37f308ee7eaa9c3300c7cc14
Tno <msa@moh-sa.dev>
2026-04-24 14:27:23 +0300

    feat: add  to axios config

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,6 +1,8 @@
 import axios from "axios";
+import { env } from "../env";
 
 export const api = axios.create({
+  baseURL: env.API_URL + "/api/v1",
   timeout: 10_000,
   headers: {
     "Content-Type": "application/json",
