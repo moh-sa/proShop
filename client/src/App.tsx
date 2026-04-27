fc470e5c222362c57212cd2ae3d498d459b06102
Tno <msa@moh-sa.dev>
2026-04-27 19:59:30 +0300

    fix(client): call `initSentry` from `App` after router is ready

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,9 @@
 import { RouterProvider } from "@tanstack/react-router";
+import { initSentry } from "./instrument";
 import getRouter from "./router";
 
 const router = getRouter();
+initSentry(router);
 
 function App() {
   return <RouterProvider router={router} />;
