6e4ac4222bf10fceb6f304ae2e78d131aa80b222
Tno <msa@moh-sa.dev>
2026-04-29 12:37:38 +0300

    chore(client): add 'eslint-plugin-package-json' to ESLint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -1,6 +1,7 @@
 import js from "@eslint/js";
 import query from "@tanstack/eslint-plugin-query";
 import eslintConfigPrettier from "eslint-config-prettier";
+import packageJson from "eslint-plugin-package-json";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
 import { defineConfig, globalIgnores } from "eslint/config";
@@ -17,6 +18,8 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
       query.configs["flat/recommended"],
+      packageJson.configs.recommended,
+      packageJson.configs.stylistic,
     ],
     languageOptions: {
       ecmaVersion: 2020,
