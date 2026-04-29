cb62846a0d70e33706f703872fe499dfe2c351cf
Tno <msa@moh-sa.dev>
2026-04-29 17:52:02 +0300

    chore(client): add HTTPs for local dev via by using `vite-plugin-mkcert`

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import { defineConfig, loadEnv } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -40,6 +41,7 @@ export default defineConfig(({ mode }) => {
 					],
 				},
 			}),
+			mkcert(),
 		],
 	};
 });
