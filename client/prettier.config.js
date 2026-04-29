b9e222489186859f19fd5f6758dc3f20cf73fcd6
Tno <msa@moh-sa.dev>
2026-04-29 11:50:45 +0300

    fix(client): correct type in Prettier config for Tailwind stylesheet

--- client/prettier.config.js
+++ client/prettier.config.js
@@ -3,7 +3,7 @@
 /** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
 const config = {
   plugins: ["prettier-plugin-tailwindcss"],
-  tailwindStyleSheet: "./src/index.css",
+  tailwindStylesheet: "./src/index.css",
 };
 
 export default config;
