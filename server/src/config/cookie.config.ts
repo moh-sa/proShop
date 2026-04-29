fc4f97b8bea3f0e8a195cd0e7a6c3765bebde6cb
Tno <msa@moh-sa.dev>
2026-04-29 17:39:59 +0300

    refactor(server): set cookie `secure` property to `true` for all environments

--- server/src/config/cookie.config.ts
+++ server/src/config/cookie.config.ts
@@ -1,11 +1,10 @@
 import type { CookieConfig } from "../types/index.js";
-import { env } from "./env.js";
 
 export const DEFAULT_COOKIE_CONFIG: CookieConfig = {
 	encode: encodeURIComponent,
 	httpOnly: true,
 	path: "/",
 	sameSite: "none",
-	secure: env.NODE_ENV === "production",
+	secure: true,
 	signed: true,
 };
