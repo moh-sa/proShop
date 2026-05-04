67e50b3bc713d9ccfeae5def8c575d33310bb3d3
Tno <msa@moh-sa.dev>
2026-05-04 17:11:44 +0300

    feat(client): wire root shell layout

--- client/src/components/layouts/root/footer.layout.tsx
+++ client/src/components/layouts/root/footer.layout.tsx
@@ -1,8 +1,25 @@
+import { Logo } from "@/components/branding";
+
 export function Footer() {
 	return (
-		<footer>
-			<div className="container mx-auto px-4 py-4 sm:px-6">
-				<p>Footer</p>
+		<footer className="border-t bg-background">
+			<div className="container mx-auto flex flex-row items-center justify-between px-4 py-4 text-muted-foreground sm:px-6">
+				<Logo className="text-lg hover:text-foreground" />
+				<div className="inline-flex items-center gap-1">
+					<span>Built by </span>
+					<a
+						className="inline-flex items-center gap-1 hover:text-foreground hover:underline"
+						href="https://github.com/moh-sa/proshop"
+						target="_blank"
+					>
+						<img
+							src="https://github.com/moh-sa.png"
+							alt="my github avatar"
+							className="size-4 rounded-full"
+						/>
+						<span>Moh-sa</span>
+					</a>
+				</div>
 			</div>
 		</footer>
 	);
--- client/src/components/layouts/root/header.layout.tsx
+++ client/src/components/layouts/root/header.layout.tsx
@@ -1,8 +1,47 @@
+import { Logo } from "@/components/branding";
+import { Skeleton } from "@/components/ui/skeleton";
+import { SignInButton } from "@/features/auth";
+import { CartButton } from "@/features/cart";
+import { SearchBar } from "@/features/search";
+import { UserMenu, type User } from "@/features/users";
+
+function AuthSection() {
+	// TODO: get user from `useAuth` hook
+	const isLoading = false;
+	const isAuthenticated = true;
+	const user: User | null = isAuthenticated
+		? {
+				id: "1",
+				name: "John Doe",
+				email: "john.doe@example.com",
+				isAdmin: true,
+				createdAt: new Date(),
+				updatedAt: new Date(),
+			}
+		: null;
+
+	if (isLoading) return <Skeleton className="size-8 rounded-full" />;
+	return user ? <UserMenu user={user} /> : <SignInButton />;
+}
+
 export function Header() {
 	return (
-		<header>
-			<div className="container mx-auto flex items-center justify-between px-4 py-4 sm:px-6">
-				Header
+		<header className="border-b bg-background">
+			<div className="container mx-auto grid grid-cols-3 grid-rows-2 items-center gap-4 px-4 py-4 sm:grid-rows-1">
+				<div className="col-span-1 col-start-1 row-start-1">
+					<Logo />
+				</div>
+
+				{/*  SEARCH BAR */}
+				<div className="col-span-full col-start-1 row-start-2 w-full sm:col-span-1 sm:col-start-2 sm:row-start-1">
+					<SearchBar />
+				</div>
+
+				{/* NAVIGATION */}
+				<nav className="col-span-1 col-start-3 row-start-1 flex items-center gap-2 justify-self-end sm:col-start-3">
+					<CartButton />
+					<AuthSection />
+				</nav>
 			</div>
 		</header>
 	);
