5cb2d638eb086c39a210b1e88250ef314a196c52
Tno <msa@moh-sa.dev>
2026-05-05 15:58:51 +0300

    refactor(client): add route guard to _admin route

--- client/src/routes/_authenticated/_admin.tsx
+++ client/src/routes/_authenticated/_admin.tsx
@@ -1,6 +1,15 @@
-import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { authKeys } from "@/features/auth";
+import type { User } from "@/features/users";
+import { createFileRoute, Outlet, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_authenticated/_admin")({
-	// TODO: add `beforeLoad` to check if has the admin permissions
+	beforeLoad({ context }) {
+		const cachedUser = context.client.getQueryData<User>(authKeys.me());
+
+		// '_authenticated' route guarantees that the user is authenticated before call reaches this route
+		if (!cachedUser!.isAdmin) {
+			throw redirect({ to: "/unauthorized", replace: true });
+		}
+	},
 	component: Outlet,
 });
