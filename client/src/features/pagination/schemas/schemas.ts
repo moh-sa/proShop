66f63f700bc53d20e00621f11fed25385e476f62
Tno <me@moh-sa.dev>
2026-05-07 00:23:03 +0300

    feat(client): add default page number and size to params schema

--- client/src/features/pagination/schemas/schemas.ts
+++ client/src/features/pagination/schemas/schemas.ts
@@ -1,6 +1,7 @@
 import z from "zod";
+import { DEFAULT_PAGE_NUMBER, DEFAULT_PAGE_SIZE } from "../consts";
 
 export const paginationParamsSchema = z.object({
-	pageNumber: z.number().int().positive(),
-	pageSize: z.number().int().positive(),
+	pageNumber: z.number().int().positive().default(DEFAULT_PAGE_NUMBER),
+	pageSize: z.number().int().positive().default(DEFAULT_PAGE_SIZE),
 });
