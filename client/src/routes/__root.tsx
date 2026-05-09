ed0bc99f030f80814fd809b994843a38801f2137
Tno <me@moh-sa.dev>
2026-05-09 12:07:05 +0300

    refactor(client): remove layout components and integrate them directly into RouteComponent

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -1,8 +1,12 @@
-import { RootLayout as RootLayoutComponent } from "@/components/layouts";
-import { useAuth } from "@/features/auth";
+import { Logo } from "@/components/branding";
+import { Skeleton } from "@/components/ui/skeleton";
+import { SignInButton, useAuth } from "@/features/auth";
+import { CartButton } from "@/features/cart";
+import { SearchBar } from "@/features/search";
+import { UserMenu } from "@/features/users";
 import { AppProvider } from "@/providers/app.provider";
 import type { RouterContext } from "@/shared/router";
-import { createRootRouteWithContext } from "@tanstack/react-router";
+import { createRootRouteWithContext, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 export const Route = createRootRouteWithContext<RouterContext>()({
@@ -13,8 +17,80 @@ function RootLayout() {
 	useAuth();
 	return (
 		<AppProvider>
-			<RootLayoutComponent />
+			<div className="grid min-h-dvh grid-rows-[auto_1fr_auto]">
+				<Header />
+				<Main />
+				<Footer />
+			</div>
 			<TanStackRouterDevtools />
 		</AppProvider>
+	);
+}
+
+// HEADER
+function AuthSection() {
+	const { isAuthenticated, isLoading, user } = useAuth();
+
+	if (isLoading) return <Skeleton className="size-8 rounded-full" />;
+	return isAuthenticated && user ? <UserMenu user={user} /> : <SignInButton />;
+}
+
+function Header() {
+	return (
+		<header className="border-b bg-background">
+			<div className="container mx-auto grid grid-cols-3 grid-rows-2 items-center gap-4 px-4 py-4 sm:grid-rows-1">
+				<div className="col-span-1 col-start-1 row-start-1">
+					<Logo />
+				</div>
+
+				{/*  SEARCH BAR */}
+				<div className="col-span-full col-start-1 row-start-2 w-full sm:col-span-1 sm:col-start-2 sm:row-start-1">
+					<SearchBar />
+				</div>
+
+				{/* NAVIGATION */}
+				<nav className="col-span-1 col-start-3 row-start-1 flex items-center gap-2 justify-self-end sm:col-start-3">
+					<CartButton />
+					<AuthSection />
+				</nav>
+			</div>
+		</header>
+	);
+}
+
+// MAIN
+function Main() {
+	return (
+		<main id="main-content">
+			<div className="container mx-auto h-full px-4 py-4 sm:px-6 sm:py-6">
+				<Outlet />
+			</div>
+		</main>
+	);
+}
+
+// FOOTER
+function Footer() {
+	return (
+		<footer className="border-t bg-background">
+			<div className="container mx-auto flex flex-row items-center justify-between px-4 py-4 text-muted-foreground sm:px-6">
+				<Logo className="text-lg hover:text-foreground" />
+				<div className="inline-flex items-center gap-1">
+					<span>Built by </span>
+					<a
+						className="inline-flex items-center gap-1 hover:text-foreground hover:underline"
+						href="https://github.com/moh-sa/proshop"
+						target="_blank"
+					>
+						<img
+							src="https://github.com/moh-sa.png"
+							alt="my github avatar"
+							className="size-4 rounded-full"
+						/>
+						<span>Moh-sa</span>
+					</a>
+				</div>
+			</div>
+		</footer>
 	);
 }
