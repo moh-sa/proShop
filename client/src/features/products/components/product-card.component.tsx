d3f7aec49aed34c7070ccba2e97f6c48f6b68cf3
Tno <me@moh-sa.dev>
2026-05-20 12:21:08 +0300

    style(products): overlay price tab on product card footer

--- client/src/features/products/components/product-card.component.tsx
+++ client/src/features/products/components/product-card.component.tsx
@@ -44,27 +44,20 @@ export function ProductCard({ product }: ProductCardProps) {
 				</Badge>
 			</div>
 
-			<FramePanel className="flex h-full shrink grow-0 flex-col justify-between gap-2 px-3 py-2">
-				<h3
-					lang="en"
-					className="my-auto grow text-2xl font-medium hyphens-auto text-black"
-				>
+			<FramePanel className="relative flex h-full shrink grow-0 flex-col justify-between gap-2 px-3 py-2">
+				<h3 lang="en" className="text-2xl font-medium hyphens-auto text-black">
 					<ProductLink productId={product.id}>{product.name}</ProductLink>
 				</h3>
-				<div className="flex shrink gap-1">
-					<span className="flex items-center justify-center rounded-lg bg-gray-100/70 px-2 py-1 text-center font-medium">
-						{formatPrice(product.price)}
-					</span>
-					<div className="grow">
-						{inStock ? (
-							<ProductCartControls item={product} className="gap-1" />
-						) : (
-							<p className="m-auto text-sm text-muted-foreground">
-								Out of stock
-							</p>
-						)}
-					</div>
-				</div>
+
+				<span className="absolute -top-1/3 left-6 inline-flex items-center justify-center rounded-t-lg border border-b-0 border-gray-300/50 bg-white px-8 pt-1.5 text-center font-medium">
+					{formatPrice(product.price)}
+				</span>
+
+				{inStock ? (
+					<ProductCartControls item={product} className="gap-1" />
+				) : (
+					<p className="m-auto text-sm text-muted-foreground">Out of stock</p>
+				)}
 			</FramePanel>
 		</Frame>
 	);
