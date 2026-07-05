c10fd40651515058a65c132837ca8b0bd9fce158
Tno <me@moh-sa.dev>
2026-07-05 16:03:31 +0300

    Comment out Sentry plugin and environment loading in Vite configuration

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,14 +1,14 @@
-import { sentryVitePlugin } from "@sentry/vite-plugin";
+// import { sentryVitePlugin } from "@sentry/vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react";
 import { resolve } from "node:path";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 import mkcert from "vite-plugin-mkcert";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-	const env = loadEnv(mode, process.cwd(), "");
+	// const env = loadEnv(mode, process.cwd(), "");
 
 	return {
 		build: { sourcemap: "hidden" },
@@ -28,19 +28,19 @@ export default defineConfig(({ mode }) => {
 					plugins: [["babel-plugin-react-compiler"]],
 				},
 			}),
-			sentryVitePlugin({
-				org: "self-yki",
-				project: "proshop-client",
-				authToken: env.VITE_SENTRY_AUTH_TOKEN,
-				telemetry: false,
-				sourcemaps: {
-					filesToDeleteAfterUpload: [
-						"./**/*.map",
-						".*/**/public/**/*.map",
-						"./dist/**/client/**/*.map",
-					],
-				},
-			}),
+			// sentryVitePlugin({
+			// 	org: "self-yki",
+			// 	project: "proshop-client",
+			// 	authToken: env.VITE_SENTRY_AUTH_TOKEN,
+			// 	telemetry: false,
+			// 	sourcemaps: {
+			// 		filesToDeleteAfterUpload: [
+			// 			"./**/*.map",
+			// 			".*/**/public/**/*.map",
+			// 			"./dist/**/client/**/*.map",
+			// 		],
+			// 	},
+			// }),
 			...(mode === "development" ? [mkcert()] : []),
 		],
 	};
